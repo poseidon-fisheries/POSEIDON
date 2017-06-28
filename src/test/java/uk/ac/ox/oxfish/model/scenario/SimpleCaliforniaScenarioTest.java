92f508e6d81bac9035461b85157c96024d4f1992
Ernesto Carrella <carrknight@gmail.com>
2017-06-28 07:44:48 +0100

    added a test to make absolutely sure markets do work if started late

--- src/test/java/uk/ac/ox/oxfish/model/scenario/SimpleCaliforniaScenarioTest.java
+++ src/test/java/uk/ac/ox/oxfish/model/scenario/SimpleCaliforniaScenarioTest.java
@@ -2,7 +2,9 @@ package uk.ac.ox.oxfish.model.scenario;
 
 import org.junit.Assert;
 import org.junit.Test;
+import uk.ac.ox.oxfish.fisher.strategies.discarding.NoDiscardingFactory;
 import uk.ac.ox.oxfish.model.FishState;
+import uk.ac.ox.oxfish.model.regs.factory.MultiITQStringFactory;
 
 /**
  * Created by carrknight on 6/26/17.
@@ -39,5 +41,33 @@ public class SimpleCaliforniaScenarioTest {
         double finalBiomass = state.getLatestYearlyObservation("Biomass Sablefish");
         System.out.println(finalBiomass/1000);
         Assert.assertEquals(finalBiomass/1000,364137.4,1);
+    }
+
+
+    @Test
+    public void marketStartsUpCorrectly() throws Exception {
+
+        SimpleCaliforniaScenario scenario = new SimpleCaliforniaScenario();
+        scenario.setUsePredictors(true);
+        scenario.setDiscardingStrategyLarge(new NoDiscardingFactory());
+        scenario.setDiscardingStrategySmall(new NoDiscardingFactory());
+        MultiITQStringFactory regs = new MultiITQStringFactory();
+        regs.setYearlyQuotaMaps("0:22222,1:55");
+        regs.setMinimumQuotaTraded("0:100,1:5");
+        scenario.setRegulationsToImposeAtStartYear(regs);
+        FishState state = new FishState(System.currentTimeMillis());
+
+        state.setScenario(scenario);
+        state.start();
+        while(state.getYear()<2)
+            state.schedule.step(state);
+        state.schedule.step(state);
+
+        Double trades = state.getLatestYearlyObservation("ITQ Volume Of Yelloweye Rockfish");
+        System.out.println(trades);
+        Assert.assertTrue(trades >0);
+
+
+
     }
 }
