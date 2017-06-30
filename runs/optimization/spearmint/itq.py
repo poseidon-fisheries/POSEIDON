82fc14523654dec573d6b340872a6a432c720b8a
carrknight <CarrKnight@gmail.com>
2017-06-30 18:52:36 +0100

    adding some tests inputs

---
+++ runs/optimization/spearmint/itq.py
@@ -0,0 +1,59 @@
+import spearfish
+import sys
+
+
+# TACS
+def build_input(filename, x, y):
+    '''
+    sets up the yaml to feed into the java model to have ITQs running and trading
+    :param filename: the name of the yaml to output
+    :param x: the numpy array with the parameters. I just expect 2: quota for first and quota for second species
+    :return: nothing
+    '''
+    import yaml
+
+    print("given " + filename)
+
+    with open("separated.yaml", 'r') as infile:
+        data = yaml.load(infile)
+
+    data["Prototype"]["regulation"] = dict()
+    data["Prototype"]["regulation"]["Multi-ITQ"] = {
+        "quotaFirstSpecie": float(x),
+        "quotaOtherSpecies": float(y)
+    }
+    # no need for predictors
+    data['Prototype']['usePredictors'] = "true"
+
+    print("ready to output " + filename)
+    print(data)
+    try:
+        with open(filename, 'w') as outfile:
+            outfile.write(str(data))
+        print("done with the output")
+    except TypeError as err:
+        print "Unexpected error:" + str(err)
+        print "Unexpected error:" + str(err.args)
+
+    print("dumped file at: " + filename)
+
+
+def scorer(yamlfile):
+    print(yamlfile)
+    return -float(yamlfile["FishState"]["Biomass Species 1"][-1]) - sum(yamlfile["FishState"]["Species 0 Landings"])
+
+
+# Write a function like this called 'main'
+def main(job_id, params):
+    print 'Anything printed here will end up in the output directory for job #%d' % job_id
+    sys.stdout.write("Starting!")
+
+    experiment_name = "tac_" + str(job_id)
+
+    print "parameters:"
+    print params
+
+    yaml_input = lambda x: build_input(x, params['x'][0], params['y'][0])
+    result = spearfish.run_experiment(yaml_input, experiment_name,scorer=scorer)
+
+    return result
