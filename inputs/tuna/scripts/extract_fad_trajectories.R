3578ea55efdfbd84138f71c829de8d6927032b5e
Nicolas Payette <nicolaspayette@gmail.com>
2019-03-07 12:46:06 +0000

    reorganise FAD trajectories extraction script and add a few plots to it

--- inputs/tuna/scripts/extract_fad_trajectories.R
+++ inputs/tuna/scripts/extract_fad_trajectories.R
@@ -1,10 +1,11 @@
+library(here)
+source(here("scripts", "download_depth_df.R"))
+
 library(tidyverse)
 library(fs)
 library(readxl)
 library(geosphere)
-library(here)
-
-source(here("scripts", "download_depth_df.R"))
+library(digest)
 
 input_path <- path_home("Dropbox", "POSEIDON", "FAD Buoy Data_Mine")
 
@@ -35,7 +36,7 @@ df_readings <-
       FLASH = col_skip(),
       VOLTAJE = col_skip(),
       TEMPERATURA = col_skip(),
-      VELOCIDAD = col_skip(),
+      VELOCIDAD = col_number(),
       RUMBO = col_skip()
     )
   )) %>%
@@ -44,7 +45,8 @@ df_readings <-
     buoy_id = as.integer(str_extract(NOMBRE, "\\d{2,}")),
     date_time = FECHA,
     lon = LONGITUD,
-    lat = LATITUD
+    lat = LATITUD,
+    velocity = VELOCIDAD * 0.514444 # convert knots to m/s
   ) %>%
   semi_join(df_events, by = "buoy_id") %>%
   mutate(buoy_id = as_factor(as.character(buoy_id)))
@@ -56,46 +58,86 @@ breaks <-
   pull(date_time) %>%
   c(min(df_readings$date_time), max(df_readings$date_time))
 
-df_with_periods <-
-  mutate(df_readings, period = cut(date_time, breaks, include.lowest = TRUE))
-
-df_with_periods %>%
-  ggplot(aes(x = date_time, y = buoy_id, color = period)) +
-  geom_point(alpha = 0.1) +
-  theme_classic()
-
-get_depth_df <- function(df, res) {
-  download_depth_df(NA, min(df$lon), min(df$lat), max(df$lon), max(df$lat), res) %>% drop_na()
-}
-depth_df <- get_depth_df(df_with_periods, .1)
-
 df <-
-  df_with_periods %>%
+  df_readings %>%
+  mutate(
+    period = cut(date_time, breaks, include.lowest = TRUE),
+    period = fct_relabel(period, ~ str_sub(., 1, 7))
+  ) %>%
   group_by(buoy_id, period) %>%
   group_map(~ .x %>%
     arrange(date_time) %>%
     rowid_to_column() %>%
-    mutate(dist = pmap_dbl(
-      list(lon, lat, lag(lon), lag(lat)),
-      ~ distHaversine(c(..1, ..2), c(..3, ..4))
-    )) %>%
-    mutate(., segment =
-      .bincode(rowid, c(-Inf, filter(., dist > 75E4)$rowid, Inf), right = FALSE)
+    mutate(.,
+      dist = pmap_dbl(list(lon, lat, lag(lon), lag(lat)), ~ distHaversine(c(..1, ..2), c(..3, ..4))),
+      i = .bincode(rowid, c(-Inf, filter(., dist > 75E4)$rowid, Inf), right = FALSE),
+      segment = map_chr(paste0(.y$buoy_id, .y$period, i), digest)
     ) %>%
-    select(date_time, lon, lat, segment)
-  ) %>%
-  ungroup() %>%
-  mutate(segment = fct_cross(buoy_id, as_factor(segment), sep = "-")) %>%
+    select(date_time, lon, lat, segment)) %>%
   group_by(segment) %>%
   filter(n() > 1) %>%
-  ungroup() %>%
-  droplevels()
+  ungroup()
+
+df %>%
+  select(segment, date_time, lon, lat) %>%
+  write_csv(here("empirical_fad_trajectories.csv"))
+
+df %>%
+  group_by(segment) %>%
+  arrange(date_time) %>%
+  summarise(
+    lon = first(lon),
+    lat = first(lat),
+    start = first(date_time),
+    duration = floor(as.numeric(difftime(last(date_time), start), units = "days"))
+  ) %>%
+  filter(duration >= 1) %>%
+  write_csv(here("fad_trajectories_validation_cases.csv"))
+
+# From here on, it's plots... -------------------------------------------------------
+
+# Read the currents file so we can get a visual comparison with the velocity readings
+df_currents <-
+  read_csv("currents.csv") %>%
+  filter(
+    between(lon, min(df_readings$lon), max(df_readings$lon)),
+    between(lat, min(df_readings$lat), max(df_readings$lat))
+  ) %>%
+  transmute(source = "currents", velocity = sqrt(u^2 + v^2))
+
+# Make a density plot of the velocity readings against the currents velocity
+df_readings %>%
+  filter(velocity <= 2) %>% # we're chucking out about 4% of readings here
+  transmute(source = "readings", velocity) %>%
+  full_join(df_currents) %>%
+  ggplot(aes(x = velocity, color = source, fill = source)) +
+  geom_density(alpha = 0.3, adjust = 2) +
+  theme_minimal()
+
+df %>%
+  ggplot(aes(x = date_time, y = buoy_id, color = period)) +
+  geom_point(alpha = 0.1) +
+  scale_color_viridis_d() +
+  guides(colour = guide_legend(override.aes = list(alpha = 1))) +
+  theme_classic()
+
+df %>%
+  ggplot(aes(x = date_time, y = buoy_id, color = segment)) +
+  geom_point(alpha = 0.5) +
+  facet_wrap(vars(period), scales = "free") +
+  guides(colour = FALSE) +
+  theme_minimal() +
+  theme(panel.grid.major.y = element_blank())
+
+depth_df <-
+  download_depth_df(NA, min(df$lon), min(df$lat), max(df$lon), max(df$lat), .1) %>%
+  drop_na()
 
 df %>%
   arrange(date_time) %>%
   ggplot(aes(x = lon, y = lat)) +
   geom_tile(data = depth_df, aes(x = x, y = y, fill = depth)) +
-  geom_path(aes(group = segment, color = as.integer(segment)), alpha = 0.6) +
+  geom_path(aes(group = segment, color = as.integer(as_factor(segment))), alpha = 0.6) +
   facet_wrap(vars(period)) +
   scale_colour_gradient(low = "yellow", high = "orange") +
   guides(fill = FALSE, colour = FALSE) +
@@ -103,5 +145,13 @@ df %>%
   theme_minimal()
 
 df %>%
-  transmute(segment = as.integer(segment), date_time, lon, lat) %>%
-  write_csv(here("fad_trajectories.csv"))
+  group_by(segment) %>%
+  filter(n() > 10) %>%
+  ungroup() %>%
+  filter(segment %in% sample(segment, 12)) %>%
+  arrange(date_time) %>%
+  ggplot(aes(x = lon, y = lat)) +
+  geom_path(aes(group = segment), alpha = 0.6) +
+  facet_wrap(vars(segment)) +
+  coord_fixed() +
+  theme_minimal()
