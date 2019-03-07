0567803a3b43cfb82f3299fc83b5c9e35c23f05e
Nicolas Payette <nicolaspayette@gmail.com>
2019-03-07 12:44:00 +0000

    extract more recent currents from SEAPODYM

--- inputs/tuna/scripts/read_SEAPODYM_current_vectors.R
+++ inputs/tuna/scripts/read_SEAPODYM_current_vectors.R
@@ -4,7 +4,7 @@ library(here)
 
 source(here("scripts", "extract_ncdf_data.R"))
 
-nc <- nc_open(here("raw", "EasternPacific_interim_historic_1x30d_19790315_PHYS.nc"))
+nc <- nc_open(here("raw", "EasternPacific_interim_historic_1x30d_20101215_PHYS.nc"))
 df <- extract_current_vectors(nc) %>% mutate(lon = lon - 360)
 nc_close(nc)
 
@@ -19,5 +19,5 @@ df %>%
   geom_tile(aes(x = lon, y = lat, fill = value)) +
   coord_fixed() +
   facet_wrap(vars(component)) +
-  scale_fill_distiller(palette = "RdBu") +
+  scale_fill_distiller(palette = "RdBu", limits = c(-1, 1)) +
   theme_minimal()
