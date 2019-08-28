449c410c822b48473d3dff84530b58516f0ec002
Nicolas Payette <nicolaspayette@gmail.com>
2019-08-28 14:24:53 +0100

    use ImmutableList in RandomFadFishingStrategy

--- src/main/java/uk/ac/ox/oxfish/fisher/strategies/fishing/RandomFadFishingStrategy.java
+++ src/main/java/uk/ac/ox/oxfish/fisher/strategies/fishing/RandomFadFishingStrategy.java
@@ -1,5 +1,6 @@
 package uk.ac.ox.oxfish.fisher.strategies.fishing;
 
+import com.google.common.collect.ImmutableList;
 import ec.util.MersenneTwisterFast;
 import org.jetbrains.annotations.NotNull;
 import uk.ac.ox.oxfish.fisher.Fisher;
@@ -13,11 +14,8 @@ import uk.ac.ox.oxfish.fisher.log.TripRecord;
 import uk.ac.ox.oxfish.model.FishState;
 import uk.ac.ox.oxfish.model.regs.Regulation;
 
-import java.util.Collection;
-import java.util.LinkedList;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static uk.ac.ox.oxfish.fisher.equipment.fads.FadManagerUtils.oneOfFadsHere;
@@ -25,12 +23,11 @@ import static uk.ac.ox.oxfish.utility.MasonUtils.oneOf;
 
 public class RandomFadFishingStrategy implements FishingStrategy, FadManagerUtils {
 
-    private final Collection<Function<Fisher, Optional<FadAction>>> actions = new LinkedList<>();
-
-    public RandomFadFishingStrategy() {
-        actions.add(fisher -> Optional.of(new DeployFad(fisher.getLocation())));
-        actions.add(fisher -> oneOfFadsHere(fisher).map(MakeFadSet::new));
-    }
+    private final ImmutableList<Function<Fisher, Optional<FadAction>>> actions =
+        ImmutableList.of(
+            fisher -> Optional.of(new DeployFad(fisher.getLocation())),
+            fisher -> oneOfFadsHere(fisher).map(MakeFadSet::new)
+        );
 
     @NotNull
     private Stream<FadAction> possibleActions(FishState model, Fisher fisher) {
@@ -43,8 +40,6 @@ public class RandomFadFishingStrategy implements FishingStrategy, FadManagerUtil
     public boolean shouldFish(
         Fisher fisher, MersenneTwisterFast random, FishState model, TripRecord currentTrip
     ) {
-        System.out.println("possibleActions(model, fisher): " + possibleActions(model, fisher).map(a -> a.toString()).collect(Collectors.joining()));
-        System.out.println("currentTrip.getEffort(): " + currentTrip.getEffort());
         return currentTrip.getEffort() <= 0 &&
             possibleActions(model, fisher).findAny().isPresent();
     }
