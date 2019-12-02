3b3ddc8c138fb7113515f4062b024cea0e2ba249
Nicolas Payette <nicolaspayette@gmail.com>
2019-12-02 11:58:51 +0000

    update tuna calibration file

--- src/main/java/uk/ac/ox/oxfish/maximization/TunaMaximization.java
+++ src/main/java/uk/ac/ox/oxfish/maximization/TunaMaximization.java
@@ -19,9 +19,8 @@ public class TunaMaximization {
     public static void main(String[] args) throws IOException {
 
         double[] optimalParameters = {
-                8.337,-1.984, 2.302,-3.536,-7.273, 10.000, 2.877,-7.130,-8.238
+            8.337, -1.984, 2.302, -3.536, -7.273, 10.000, 2.877, -7.130, -8.238
         };
-
 
         FishYAML yaml = new FishYAML();
         Path optimizationFile = Paths.get("inputs", "tuna", "calibration.yaml");
@@ -31,8 +30,7 @@ public class TunaMaximization {
         Path outputFile = optimizationFile.getParent().resolve("tuna_calibrated.yaml");
         yaml.dump(scenario, new FileWriter(outputFile.toFile()));
 
-
-        ScaledFixedDataLastStepTarget.VERBOSE=true;
+        ScaledFixedDataLastStepTarget.VERBOSE = true;
         optimization.evaluate(optimalParameters);
 
     }
