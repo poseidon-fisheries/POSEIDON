3cc9f03e200b6d78d7ece7657d9652eba49e10bd
Nicolas Payette <nicolaspayette@gmail.com>
2020-03-09 12:39:29 +0000

    increase test coverage of FadManagerUtils

--- src/main/java/uk/ac/ox/oxfish/fisher/equipment/fads/FadManagerUtils.java
+++ src/main/java/uk/ac/ox/oxfish/fisher/equipment/fads/FadManagerUtils.java
@@ -1,3 +1,22 @@
+/*
+ *  POSEIDON, an agent-based model of fisheries
+ *  Copyright (C) 2020  CoHESyS Lab cohesys.lab@gmail.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package uk.ac.ox.oxfish.fisher.equipment.fads;
 
 import org.apache.commons.collections15.set.ListOrderedSet;
@@ -20,6 +39,13 @@ import static uk.ac.ox.oxfish.utility.MasonUtils.oneOf;
  */
 public interface FadManagerUtils {
 
+    static Optional<Fad> oneOfDeployedFads(Fisher fisher) {
+        final ListOrderedSet<Fad> deployedFads = getFadManager(fisher).getDeployedFads();
+        return deployedFads.isEmpty() ?
+            Optional.empty() :
+            Optional.of(oneOf(deployedFads, fisher.grabRandomizer()));
+    }
+
     static FadManager getFadManager(Fisher fisher) {
         if (fisher.getGear() instanceof PurseSeineGear)
             return ((PurseSeineGear) fisher.getGear()).getFadManager();
@@ -28,18 +54,6 @@ public interface FadManagerUtils {
                 fisher + " is using " + fisher.getGear().getClass() + "."
         );
     }
-
-    static Optional<Fad> oneOfFadsHere(Fisher fisher) {
-        return getFadManager(fisher).oneOfFadsHere();
-    }
-
-    static Optional<Fad> oneOfDeployedFads(Fisher fisher) {
-        final ListOrderedSet<Fad> deployedFads = getFadManager(fisher).getDeployedFads();
-        return deployedFads.isEmpty() ?
-            Optional.empty() :
-            Optional.of(oneOf(deployedFads, fisher.grabRandomizer()));
-    }
-
     static Stream<Fad> fadsHere(Fisher fisher) { return bagToStream(getFadManager(fisher).getFadsHere()); }
 
     static Stream<Fad> fadsAt(Fisher fisher, SeaTile seaTile) {
