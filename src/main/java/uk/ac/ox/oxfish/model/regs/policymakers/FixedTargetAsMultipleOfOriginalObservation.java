abebfe9a5ee036a12a4fc1d2b059c1cc6a4398f5
carrknight <carrknight@gmail.com>
2020-06-08 10:41:12 +0100

    an ITarget-like sensor for targeting so I can use it for standard PI controllers

---
+++ src/main/java/uk/ac/ox/oxfish/model/regs/policymakers/FixedTargetAsMultipleOfOriginalObservation.java
@@ -0,0 +1,109 @@
+package uk.ac.ox.oxfish.model.regs.policymakers;
+
+import uk.ac.ox.oxfish.model.FishState;
+import uk.ac.ox.oxfish.model.data.collectors.DataColumn;
+import uk.ac.ox.oxfish.utility.adaptation.Sensor;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.Iterator;
+import java.util.function.Function;
+
+/**
+ * mouthful for something relatively simple:
+ * takes the ITarget way of setting a target: average indicator in the past X years
+ * times a multiplier.
+ * Once that's set, the sensor never updates its target
+ */
+public class FixedTargetAsMultipleOfOriginalObservation implements
+        Sensor<FishState,Double> {
+
+
+    private final String indicatorColumnName;
+
+
+    /**
+     * pipe observations through these, useful for rescaling: THIS HAPPENS BEFORE LOGGINg IT
+     */
+    private Function<Double,Double> indicatorTransformer = indicator -> indicator;
+
+    /**
+     * Itarget is IAverage*IndicatorMultiplier
+     */
+    private final double indicatorMultiplier;
+
+    /**
+     * how many years back to go
+     */
+    private final int yearsToLookBack;
+
+    private double targetSet = Double.NaN;
+
+    public FixedTargetAsMultipleOfOriginalObservation(String indicatorColumnName, double indicatorMultiplier, int yearsToLookBack) {
+        this.indicatorColumnName = indicatorColumnName;
+        this.indicatorMultiplier = indicatorMultiplier;
+        this.yearsToLookBack = yearsToLookBack;
+    }
+
+    @Override
+    public Double scan(FishState system) {
+
+        //if you have already set the target, go no further
+        if(Double.isFinite(targetSet))
+            return targetSet;
+
+        final DataColumn indicatorColumn = system.getYearlyDataSet().getColumn(indicatorColumnName);
+        //you need to have at least tyearsToLookBack observations
+        if(indicatorColumn.size()<yearsToLookBack) //need a long enough time series!
+            return Double.NaN;
+
+        final Iterator<Double> indicatorIterator = indicatorColumn.descendingIterator();
+        DoubleSummaryStatistics indicators = new DoubleSummaryStatistics();
+
+
+        for (int lag = 0; lag < yearsToLookBack;
+             lag++) {
+
+            //transforming them if necessary
+            final Double observedIndicator = indicatorTransformer.apply(
+                    indicatorIterator.next());
+            indicators.accept(observedIndicator);
+        }
+
+        // get the target
+
+        double indicatorAve = indicators.getAverage();
+        targetSet = indicatorAve* indicatorMultiplier;
+        return targetSet;
+
+
+    }
+
+
+    public String getIndicatorColumnName() {
+        return indicatorColumnName;
+    }
+
+    public Function<Double, Double> getIndicatorTransformer() {
+        return indicatorTransformer;
+    }
+
+    public void setIndicatorTransformer(Function<Double, Double> indicatorTransformer) {
+        this.indicatorTransformer = indicatorTransformer;
+    }
+
+    public double getIndicatorMultiplier() {
+        return indicatorMultiplier;
+    }
+
+    public int getYearsToLookBack() {
+        return yearsToLookBack;
+    }
+
+    public double getTargetSet() {
+        return targetSet;
+    }
+
+    public void setTargetSet(double targetSet) {
+        this.targetSet = targetSet;
+    }
+}
---
+++ src/test/java/uk/ac/ox/oxfish/model/regs/policymakers/FixedTargetAsMultipleOfOriginalObservationTest.java
@@ -0,0 +1,38 @@
+package uk.ac.ox.oxfish.model.regs.policymakers;
+
+import org.junit.Test;
+import uk.ac.ox.oxfish.model.FishState;
+import uk.ac.ox.oxfish.model.data.collectors.DataColumn;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class FixedTargetAsMultipleOfOriginalObservationTest {
+
+    @Test
+    public void targetIsSetCorrectly() {
+
+        //these number come from the DLMTookkit, SimulatedData, row 1
+        final double[] indicators = {0.727146523713908, 1.00488900317951, 1.05670327078653, 1.1620629858966,
+                0.701410061340196, 0.914689667756402,
+                //only these three matter
+                0.85, 1.20, 0.75};
+
+
+        DataColumn indicatorColumn = new DataColumn("indicator");
+        for (double indicator : indicators) {
+            indicatorColumn.add(indicator);
+        }
+
+
+        FishState state = mock(FishState.class,RETURNS_DEEP_STUBS);
+        when(state.getYearlyDataSet().getColumn("indicator")).thenReturn(indicatorColumn);
+
+        FixedTargetAsMultipleOfOriginalObservation target =
+                new FixedTargetAsMultipleOfOriginalObservation("indicator",
+                        1.5,3);
+        assertEquals(target.scan(state),
+                1.4,.0001d);
+
+    }
+}
