c1b63f8aa36d5d73259e872fa1c187b5cd5d0498
carrknight <carrknight@gmail.com>
2020-07-20 07:53:31 +0100

    created a few more data collectors to compare perfect knowledge (wrt boats and life history parameters)

--- src/main/java/uk/ac/ox/oxfish/experiments/indonesia/limited/NoData718Slice6Policy.java
+++ src/main/java/uk/ac/ox/oxfish/experiments/indonesia/limited/NoData718Slice6Policy.java
@@ -1,10 +1,18 @@
 package uk.ac.ox.oxfish.experiments.indonesia.limited;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 import com.opencsv.CSVReader;
 import sim.engine.SimState;
 import sim.engine.Steppable;
+import uk.ac.ox.oxfish.biology.Species;
+import uk.ac.ox.oxfish.biology.boxcars.SPRAgentBuilder;
+import uk.ac.ox.oxfish.biology.initializer.factory.MultipleIndependentSpeciesAbundanceFactory;
+import uk.ac.ox.oxfish.biology.initializer.factory.SingleSpeciesBoxcarFactory;
 import uk.ac.ox.oxfish.experiments.indonesia.Slice6Sweeps;
+import uk.ac.ox.oxfish.fisher.Fisher;
+import uk.ac.ox.oxfish.geography.NauticalMap;
+import uk.ac.ox.oxfish.geography.SeaTile;
 import uk.ac.ox.oxfish.geography.ports.Port;
 import uk.ac.ox.oxfish.model.AdditionalStartable;
 import uk.ac.ox.oxfish.model.FishState;
@@ -12,19 +20,22 @@ import uk.ac.ox.oxfish.model.StepOrder;
 import uk.ac.ox.oxfish.model.market.FixedPriceMarket;
 import uk.ac.ox.oxfish.model.market.Market;
 import uk.ac.ox.oxfish.model.market.MarketProxy;
+import uk.ac.ox.oxfish.model.regs.ProtectedAreasOnly;
+import uk.ac.ox.oxfish.model.regs.factory.ProtectedAreasOnlyFactory;
+import uk.ac.ox.oxfish.model.scenario.FisherFactory;
 import uk.ac.ox.oxfish.model.scenario.FlexibleScenario;
 import uk.ac.ox.oxfish.model.scenario.Scenario;
 import uk.ac.ox.oxfish.utility.AlgorithmFactory;
 import uk.ac.ox.oxfish.utility.FishStateUtilities;
+import uk.ac.ox.oxfish.utility.parameters.FixedDoubleParameter;
+import uk.ac.ox.oxfish.utility.yaml.FishYAML;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -33,7 +44,7 @@ public class NoData718Slice6Policy {
     public static final String CANDIDATES_CSV_FILE = "total_successes.csv";
     public static final int SEED = 0;
     private static Path OUTPUT_FOLDER =
-            NoData718Slice6.MAIN_DIRECTORY.resolve("outputs");
+            NoData718Slice6.MAIN_DIRECTORY.resolve("outputs_complete");
 
 
 
@@ -106,6 +117,116 @@ public class NoData718Slice6Policy {
     }
 
 
+    private static Consumer<Scenario> protectBestCell(int shockYear){
+        return new Consumer<Scenario>() {
+            @Override
+            public void accept(Scenario scenario) {
+                ((FlexibleScenario) scenario).getPlugins().add(
+                        new AlgorithmFactory<AdditionalStartable>() {
+                            @Override
+                            public AdditionalStartable apply(FishState state) {
+
+                                return new AdditionalStartable() {
+                                    @Override
+                                    public void start(FishState model) {
+
+                                        model.scheduleOnceAtTheBeginningOfYear(
+                                                new Steppable() {
+                                                    @Override
+                                                    public void step(SimState simState) {
+
+                                                        //go through all possible tiles; find the one that has now the most atrobucca
+                                                        //protected it!
+                                                        final FishState model = (FishState) simState;
+                                                        final Species brevis = model.getSpecies("Atrobucca Brevis");
+                                                        final SeaTile toProtect = model.getMap().getAllSeaTilesExcludingLandAsList().stream().max(
+                                                                new Comparator<SeaTile>() {
+                                                                    @Override
+                                                                    public int compare(SeaTile thisTile,
+                                                                                       SeaTile thatTile) {
+                                                                        return Double.compare(
+                                                                                thisTile.getBiomass(brevis),
+                                                                                thatTile.getBiomass(brevis)
+
+                                                                        );
+                                                                    }
+                                                                }
+                                                        ).get();
+                                                        toProtect.assignMpa(NauticalMap.MPA_SINGLETON);
+
+                                                        //now go through all fishers and make them follow MPA
+                                                        for (Fisher fisher : model.getFishers()) {
+                                                            fisher.setRegulation(new ProtectedAreasOnly());
+                                                        }
+                                                        for (Map.Entry<String, FisherFactory> fisherFactory : model.getFisherFactories()) {
+                                                            fisherFactory.getValue().setRegulations(
+                                                                    new ProtectedAreasOnlyFactory()
+                                                            );
+                                                        }
+
+                                                    }
+                                                }, StepOrder.DAWN, shockYear);
+
+                                    }
+                                };
+
+
+                            }
+                        });
+            }
+        };
+    }
+
+    static public LinkedHashMap<String, Function<Integer, Consumer<Scenario>>> policiesMPA = new LinkedHashMap();
+
+
+
+
+    static{
+        policiesMPA.put(
+                "BAU",
+                shockYear -> scenario -> {
+                }
+
+        );
+
+        policiesMPA.put(
+                "MPA_entry",
+                shockYear -> protectBestCell(shockYear)
+
+
+        );
+
+
+        policiesMPA.put(
+                "MPA_noentry",
+                shockYear -> protectBestCell(shockYear).andThen(NoDataPolicy.removeEntry(shockYear))
+
+
+        );
+
+
+        for(int days = 250; days>=100; days-=10) {
+            int finalDays = days;
+            policiesMPA.put(
+                    days+"_days_MPA_noentry",
+                    //max days regulations include respect protected areas so it works if put in this order
+                    shockYear ->  protectBestCell(shockYear).andThen(NoDataPolicy.buildMaxDaysRegulation(shockYear,
+                            new String[]{"population0", "population1", "population2"}
+                            , finalDays).andThen(
+                            NoDataPolicy.removeEntry(shockYear)
+                            )
+                    )
+
+            );
+        }
+
+
+
+
+
+    }
+
 
     static {
 
@@ -165,7 +286,17 @@ public class NoData718Slice6Policy {
 
     }
 
+    static public LinkedHashMap<String, Function<Integer, Consumer<Scenario>>> onlyBAU = new LinkedHashMap();
 
+    static {
+
+        onlyBAU.put(
+                "BAU",
+                shockYear -> scenario -> {
+                }
+
+        );
+    }
 
 
     public static void main(String[] args) throws IOException {
@@ -173,7 +304,7 @@ public class NoData718Slice6Policy {
         runPolicyDirectory(
                 OUTPUT_FOLDER.getParent().resolve(CANDIDATES_CSV_FILE).toFile(),
                 OUTPUT_FOLDER,
-                policies);
+                onlyBAU);
 
 
     }
@@ -198,6 +329,52 @@ public class NoData718Slice6Policy {
     }
 
 
+    //additional data collectors
+    private static final List<String> ADDITIONAL_PLUGINS =
+            Lists.newArrayList(
+                    "- SPR Fixed Sample Agent:\n" +
+                            "    assumedKParameter: '0.322'\n" +
+                            "    assumedLengthAtMaturity: '29.0'\n" +
+                            "    assumedLengthBinCm: '5.0'\n" +
+                            "    assumedLinf: '59.0'\n" +
+                            "    assumedNaturalMortality: '0.495'\n" +
+                            "    assumedVarA: '0.0197'\n" +
+                            "    assumedVarB: '2.99'\n" +
+                            "    simulatedMaxAge: '100.0'\n" +
+                            "    simulatedVirginRecruits: '1000.0'\n" +
+                            "    speciesName: Lethrinus laticaudis\n" +
+                            "    surveyTag: spr_agent1_total\n" +
+                            "    probabilityOfSamplingEachBoat: 1",
+                    "- SPR Fixed Sample Agent:\n" +
+                            "    assumedKParameter: '0.4438437'\n" +
+                            "    assumedLengthAtMaturity: '50.0'\n" +
+                            "    assumedLengthBinCm: '5.0'\n" +
+                            "    assumedLinf: '86.0'\n" +
+                            "    assumedNaturalMortality: '0.3775984'\n" +
+                            "    assumedVarA: '0.00853'\n" +
+                            "    assumedVarB: '3.137'\n" +
+                            "    simulatedMaxAge: '100.0'\n" +
+                            "    simulatedVirginRecruits: '1000.0'\n" +
+                            "    speciesName: Lutjanus malabaricus\n" +
+                            "    surveyTag: spr_agent2_total\n" +
+                            "    probabilityOfSamplingEachBoat: 1",
+                    "- SPR Fixed Sample Agent:\n" +
+                            "    assumedKParameter: '0.291'\n" +
+                            "    assumedLengthAtMaturity: '34.0'\n" +
+                            "    assumedLengthBinCm: '5.0'\n" +
+                            "    assumedLinf: '68.0'\n" +
+                            "    assumedNaturalMortality: '0.447'\n" +
+                            "    assumedVarA: '0.0128'\n" +
+                            "    assumedVarB: '2.94'\n" +
+                            "    simulatedMaxAge: '100.0'\n" +
+                            "    simulatedVirginRecruits: '1000.0'\n" +
+                            "    speciesName: Atrobucca brevis\n" +
+                            "    surveyTag: spr_agent3_total\n" +
+                            "    probabilityOfSamplingEachBoat: 1"
+
+
+            );
+
     private static void runOnePolicySimulation(Path scenarioFile,
                                                int yearOfPriceShock,
                                                int yearOfPolicyShock,
@@ -212,20 +389,73 @@ public class NoData718Slice6Policy {
             final String agent = NoData718Slice2PriceIncrease.speciesToSprAgent.get(species);
             Preconditions.checkNotNull(agent, "species has no agent!");
             additionalColumns.add("SPR " + species + " " + agent + "_small");
+            additionalColumns.add("SPR " + species + " " + agent + "_total");
         }
+        additionalColumns.add("Exogenous catches of Lutjanus malabaricus");
+        additionalColumns.add("Exogenous catches of Lethrinus laticaudis");
+        additionalColumns.add("Exogenous catches of Atrobucca brevis");
+        additionalColumns.add("Others Landings");
+        additionalColumns.add("Others Earnings");
+        additionalColumns.add("SPR " + "Lutjanus malabaricus" + " " +"total_and_correct");
+
+
+        FishYAML yaml = new FishYAML();
+
+        final List<AlgorithmFactory<? extends AdditionalStartable>> plugins = new LinkedList<>();
+        for (String additionalPlugin : ADDITIONAL_PLUGINS) {
+            plugins.add(
+                    yaml.loadAs(additionalPlugin,AlgorithmFactory.class)
+            );
+        }
+
+
+
         NoData718Slice4PriceIncrease.priceIncreaseOneRun(
                 scenarioFile,
-                yearOfPolicyShock,
+                yearOfPolicyShock + 1,
                 outputFolder,
                 policies,
-                null,
+                additionalColumns,
                 true,
                 NoData718Slice4PriceIncrease.priceShockAndSeedingGenerator(0).
-                        apply(yearOfPriceShock)
+                        apply(yearOfPriceShock),
+                new Consumer<Scenario>() {
+                    @Override
+                    public void accept(Scenario scenario) {
+                        ((FlexibleScenario) scenario).getPlugins().addAll(plugins);
+                    }
+                },
+                CORRECT_LIFE_HISTORIES_CONSUMER
 
         );
 
 
     }
+
+
+
+    private static final Consumer<Scenario> CORRECT_LIFE_HISTORIES_CONSUMER =
+            new Consumer<Scenario>() {
+                @Override
+                public void accept(Scenario scenario) {
+
+                    final FlexibleScenario flexible = (FlexibleScenario) scenario;
+                    final SingleSpeciesBoxcarFactory malabaricus = (SingleSpeciesBoxcarFactory) ((MultipleIndependentSpeciesAbundanceFactory) flexible.getBiologyInitializer()).getFactories().
+                            get(1);
+                    Preconditions.checkArgument(malabaricus.getSpeciesName().equals("Lutjanus malabaricus"));
+                    SPRAgentBuilder builder = new SPRAgentBuilder();
+                    builder.setAssumedKParameter(malabaricus.getK().makeCopy());
+                    builder.setAssumedLengthAtMaturity(malabaricus.getLengthAtMaturity().makeCopy());
+                    builder.setAssumedLinf(malabaricus.getLInfinity().makeCopy());
+                    builder.setAssumedNaturalMortality(malabaricus.getYearlyMortality().makeCopy());
+                    builder.setAssumedVarA(malabaricus.getAllometricAlpha().makeCopy());
+                    builder.setAssumedVarB(malabaricus.getAllometricBeta().makeCopy());
+
+                    builder.setSurveyTag("total_and_correct");
+                    builder.setProbabilityOfSamplingEachBoat(new FixedDoubleParameter(1));
+
+                    ((FlexibleScenario) scenario).getPlugins().add(builder);
+                }
+            };
 
 }
