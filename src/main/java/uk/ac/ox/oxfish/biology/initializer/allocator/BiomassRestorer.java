c63eb487556b522050565937e1ec139082e9d4d4
Nicolas Payette <nicolaspayette@gmail.com>
2021-07-30 13:19:01 +0100

    extract Restorer superclass so we can use is with the AbundanceRestorer

--- src/main/java/uk/ac/ox/oxfish/biology/initializer/allocator/BiomassRestorer.java
+++ src/main/java/uk/ac/ox/oxfish/biology/initializer/allocator/BiomassRestorer.java
@@ -5,7 +5,6 @@ import static java.util.function.Function.identity;
 import static uk.ac.ox.oxfish.model.StepOrder.DAWN;
 import static uk.ac.ox.oxfish.model.StepOrder.POLICY_UPDATE;
 
-import com.google.common.collect.ImmutableMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import org.apache.logging.log4j.LogManager;
@@ -15,22 +14,16 @@ import uk.ac.ox.oxfish.biology.GlobalBiology;
 import uk.ac.ox.oxfish.biology.Species;
 import uk.ac.ox.oxfish.geography.NauticalMap;
 import uk.ac.ox.oxfish.geography.fads.FadMap;
-import uk.ac.ox.oxfish.model.AdditionalStartable;
 import uk.ac.ox.oxfish.model.FishState;
-import uk.ac.ox.oxfish.model.StepOrder;
-import uk.ac.ox.oxfish.utility.FishStateSteppable;
 
 /**
  * This class does a similar job as the various implementations of {@link
  * uk.ac.ox.oxfish.biology.complicated.BiologyResetter}, but it relies on a {@link
  * BiomassReallocator} and deals with all species at once.
  */
-public class BiomassRestorer implements AdditionalStartable {
+public class BiomassRestorer extends Restorer<BiomassReallocator, Double> {
 
     private static final Logger logger = LogManager.getLogger("biomass_events");
-
-    private final BiomassReallocator biomassReallocator;
-    private final Map<Integer, Integer> schedule;
 
     /**
      * Create a new BiomassRestorer.
@@ -44,40 +37,10 @@ public class BiomassRestorer implements AdditionalStartable {
         final BiomassReallocator biomassReallocator,
         final Map<Integer, Integer> schedule
     ) {
-        this.biomassReallocator = biomassReallocator;
-        this.schedule = ImmutableMap.copyOf(schedule);
+        super(biomassReallocator, schedule);
     }
 
-    @Override
-    public void start(final FishState fishState) {
-        schedule.forEach((recordingStep, restoringStep) ->
-            // record the biomass at dawn, before anything else happens
-            schedule(fishState, recordingStep, DAWN, fishState1 -> {
-                System.out.println("Taking biomass record at step " + fishState1.getStep());
-                final Map<Species, Double> recordedBiomass = recordBiomass(fishState1);
-                // Schedule the biomass restoration at POLICY_UPDATE step order so it runs after the
-                // grower at BIOLOGY_PHASE step order, but before the data gatherers at later orders
-                schedule(fishState1, restoringStep, POLICY_UPDATE, fishState2 -> {
-                    System.out
-                        .printf("Restoring biomass recorded at step %d at step %d\n", recordingStep,
-                            fishState2.getStep()
-                        );
-                    restoreBiomass(recordedBiomass, fishState2);
-                });
-            })
-        );
-    }
-
-    private static void schedule(
-        final FishState fishState,
-        final int step,
-        final StepOrder stepOrder,
-        @SuppressWarnings("TypeMayBeWeakened") final FishStateSteppable stepper
-    ) {
-        fishState.schedule.scheduleOnce(step, stepOrder.ordinal(), stepper);
-    }
-
-    private static Map<Species, Double> recordBiomass(final FishState fishState) {
+     @Override Map<Species, Double> record(final FishState fishState) {
 
         final Map<Species, Double> recordedBiomass = fishState.getTotalBiomasses();
 
@@ -94,8 +57,8 @@ public class BiomassRestorer implements AdditionalStartable {
         return recordedBiomass;
     }
 
-    private void restoreBiomass(
-        final Map<Species, Double> recordedBiomass,
+    @Override void restore(
+        final Map<Species, Double> records,
         final FishState fishState
     ) {
         final GlobalBiology globalBiology = fishState.getBiology();
@@ -112,7 +75,7 @@ public class BiomassRestorer implements AdditionalStartable {
             ));
 
         final Map<Species, Double> biomassToReallocate =
-            recordedBiomass.entrySet().stream()
+            records.entrySet().stream()
                 .collect(toImmutableMap(
                     Entry::getKey,
                     entry -> entry.getValue() - biomassUnderFads.get(entry.getKey())
@@ -120,7 +83,7 @@ public class BiomassRestorer implements AdditionalStartable {
 
         final Map<Species, Double> biomassBefore = fishState.getTotalBiomasses();
 
-        biomassReallocator.reallocate(
+        reallocator.reallocate(
             fishState,
             globalBiology,
             nauticalMap,
@@ -137,8 +100,5 @@ public class BiomassRestorer implements AdditionalStartable {
                 biomassAfter
             ))
         );
-
     }
-
-
 }
