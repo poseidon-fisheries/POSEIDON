0ab8ec82106a02af2656365957c7d8e7c3adafd2
Brian Powers <brpowers@asu.edu>
2022-01-12 14:14:54 -0700

    AbundanceReallocator Test Passed, moving on to the next

--- src/test/java/uk/ac/ox/oxfish/biology/tuna/AbundanceReallocatorTest.java
+++ src/test/java/uk/ac/ox/oxfish/biology/tuna/AbundanceReallocatorTest.java
@@ -1,11 +1,13 @@
 package uk.ac.ox.oxfish.biology.tuna;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSortedMap;
 import org.junit.Test;
 import sim.field.grid.DoubleGrid2D;
 import uk.ac.ox.oxfish.biology.BiomassLocalBiology;
 import uk.ac.ox.oxfish.biology.GlobalBiology;
 import uk.ac.ox.oxfish.biology.Species;
+import uk.ac.ox.oxfish.biology.complicated.AbundanceLocalBiology;
 import uk.ac.ox.oxfish.biology.tuna.SmallLargeAllocationGridsSupplier.SizeGroup;
 import uk.ac.ox.oxfish.geography.NauticalMap;
 import uk.ac.ox.oxfish.geography.MapExtent;
@@ -42,9 +44,9 @@ public class AbundanceReallocatorTest {
         SpeciesCodes speciesCodes = new SpeciesCodes(sCodes);
 
         Map<String, IntFunction<SizeGroup>> binToSizeGroupMappings = new HashMap<>();
-        binToSizeGroupMappings.put("SP1", entry -> entry==0?SMALL:LARGE );
+        binToSizeGroupMappings.put("Piano Tuna", entry -> entry==0?SMALL:LARGE );
 
-        ImmutableList<DoubleGrid2D> grids = Stream
+/*        ImmutableList<DoubleGrid2D> grids = Stream
                 .of(
                         new double[][] {{1, 1, 1}, {0, 0, 0}, {0, 0, 0}},
                         new double[][] {{0, 0, 0}, {1, 1, 1}, {0, 0, 0}},
@@ -52,27 +54,51 @@ public class AbundanceReallocatorTest {
                 )
                 .map(DoubleGrid2D::new)
                 .map(AllocationGridsSupplier::normalize)
-                .collect(toImmutableList());
-        final GlobalBiology globalBiology = genericListOfSpecies(2);
+                .collect(toImmutableList());*/
+        final GlobalBiology globalBiology= new GlobalBiology(species1);
+        HashMap<Species, double[][]> abundance = new HashMap<>();
+        abundance.put(species1, new double[][]{{10, 10}, {10, 10}});
+
         final NauticalMap nauticalMap = makeMap(3, 3);
-        nauticalMap.getAllSeaTilesAsList().forEach(seaTile ->
-                seaTile.setBiology(new BiomassLocalBiology(
-                                new double[] {1, 1},
-                                new double[] {POSITIVE_INFINITY, POSITIVE_INFINITY}
-                        )
+         nauticalMap.getAllSeaTilesAsList().forEach(seaTile ->
+                seaTile.setBiology(new AbundanceLocalBiology(abundance)
                 )
         );
 
         Path INPUT_PATH = Paths.get("inputs", "epo");
+        AllocationGrids<Entry<String, SizeGroup>> allocationGrids = new SmallLargeAllocationGridsSupplier(
+                speciesCodes,
+                INPUT_PATH.resolve("test").resolve("mock_grids.csv")   ,
+                new MapExtent(nauticalMap),
+                365).get();
 
         AbundanceReallocator reallocator = new AbundanceReallocator(
-                new SmallLargeAllocationGridsSupplier(
-                        speciesCodes,
-                        INPUT_PATH.resolve("test").resolve("mock_grids.csv")   ,
-                        new MapExtent(nauticalMap),
-                        365).get(),
+                allocationGrids,
                 binToSizeGroupMappings
         );
+
+
+
+
+        ImmutableSortedMap<Integer, Map<Entry<String, SizeGroup>, DoubleGrid2D>> grids = allocationGrids.getGrids();
+        AbundanceLocalBiology aggregatedBiology = new AbundanceLocalBiology(abundance);
+
+
+//        System.out.println("break");
+        reallocator.reallocate(grids.get(0), globalBiology, nauticalMap.getAllSeaTilesAsList(), aggregatedBiology);
+
+        assertEquals(
+                0.991991733,
+                nauticalMap.getAllSeaTilesAsList().get(0).getAbundance(species1).asMatrix()[0][0],
+                .0000001
+        );
+
+        assertEquals(
+                1.13292204,
+                nauticalMap.getAllSeaTilesAsList().get(4).getAbundance(species1).asMatrix()[0][1],
+                .0000001
+        );
+
 
     }
 
