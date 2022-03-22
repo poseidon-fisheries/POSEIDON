0c1029daf3011423b209d667cdb041c96ac7f265
Nicolas Payette <nicolaspayette@gmail.com>
2022-03-22 15:20:31 +0000

    fix CompressedExponentialFunctionTest

--- src/test/java/uk/ac/ox/oxfish/fisher/purseseiner/utils/CompressedExponentialFunctionTest.java
+++ src/test/java/uk/ac/ox/oxfish/fisher/purseseiner/utils/CompressedExponentialFunctionTest.java
@@ -27,49 +27,49 @@ import uk.ac.ox.oxfish.utility.operators.CompressedExponentialFunction;
 public class CompressedExponentialFunctionTest extends TestCase {
 
     public void testApplyAsDouble() {
-        final DoubleUnaryOperator f1 = new CompressedExponentialFunction(0, 0);
-        assertEquals(0.632120558828558, f1.applyAsDouble(0), EPSILON);
-        assertEquals(0.632120558828558, f1.applyAsDouble(1), EPSILON);
-        assertEquals(0.632120558828558, f1.applyAsDouble(2), EPSILON);
+        final DoubleUnaryOperator f1 = new CompressedExponentialFunction(0, 2);
+        assertEquals(0, f1.applyAsDouble(0), EPSILON);
+        assertEquals(0, f1.applyAsDouble(1), EPSILON);
+        assertEquals(0, f1.applyAsDouble(2), EPSILON);
 
-        final DoubleUnaryOperator f2 = new CompressedExponentialFunction(0, 1);
+        final DoubleUnaryOperator f2 = new CompressedExponentialFunction(0, 3);
         assertEquals(0, f2.applyAsDouble(0), EPSILON);
         assertEquals(0, f2.applyAsDouble(1), EPSILON);
         assertEquals(0, f2.applyAsDouble(2), EPSILON);
 
-        final DoubleUnaryOperator f3 = new CompressedExponentialFunction(0, 2);
+        final DoubleUnaryOperator f3 = new CompressedExponentialFunction(0, 4);
         assertEquals(0, f3.applyAsDouble(0), EPSILON);
         assertEquals(0, f3.applyAsDouble(1), EPSILON);
         assertEquals(0, f3.applyAsDouble(2), EPSILON);
 
-        final DoubleUnaryOperator f4 = new CompressedExponentialFunction(0.5, 0);
-        assertEquals(0.632120558828558, f4.applyAsDouble(0), EPSILON);
-        assertEquals(0.632120558828558, f4.applyAsDouble(1), EPSILON);
-        assertEquals(0.632120558828558, f4.applyAsDouble(2), EPSILON);
+        final DoubleUnaryOperator f4 = new CompressedExponentialFunction(0.5, 2);
+        assertEquals(0, f4.applyAsDouble(0), EPSILON);
+        assertEquals(0.2211, f4.applyAsDouble(1), EPSILON);
+        assertEquals(0.6321, f4.applyAsDouble(2), EPSILON);
 
-        final DoubleUnaryOperator f5 = new CompressedExponentialFunction(0.5, 1);
+        final DoubleUnaryOperator f5 = new CompressedExponentialFunction(0.5, 3);
         assertEquals(0, f5.applyAsDouble(0), EPSILON);
-        assertEquals(0.393469340287367, f5.applyAsDouble(1), EPSILON);
-        assertEquals(0.632120558828558, f5.applyAsDouble(2), EPSILON);
+        assertEquals(0.1175, f5.applyAsDouble(1), EPSILON);
+        assertEquals(0.6321, f5.applyAsDouble(2), EPSILON);
 
-        final DoubleUnaryOperator f6 = new CompressedExponentialFunction(0.5, 2);
+        final DoubleUnaryOperator f6 = new CompressedExponentialFunction(0.5, 4);
         assertEquals(0, f6.applyAsDouble(0), EPSILON);
-        assertEquals(0.221199216928595, f6.applyAsDouble(1), EPSILON);
-        assertEquals(0.632120558828558, f6.applyAsDouble(2), EPSILON);
+        assertEquals(0.0605, f6.applyAsDouble(1), EPSILON);
+        assertEquals(0.6321, f6.applyAsDouble(2), EPSILON);
 
-        final DoubleUnaryOperator f7 = new CompressedExponentialFunction(1, 0);
-        assertEquals(0.632120558828558, f7.applyAsDouble(0), EPSILON);
-        assertEquals(0.632120558828558, f7.applyAsDouble(1), EPSILON);
-        assertEquals(0.632120558828558, f7.applyAsDouble(2), EPSILON);
+        final DoubleUnaryOperator f7 = new CompressedExponentialFunction(1, 2);
+        assertEquals(0, f7.applyAsDouble(0), EPSILON);
+        assertEquals(0.6321, f7.applyAsDouble(1), EPSILON);
+        assertEquals(0.9816844, f7.applyAsDouble(2), EPSILON);
 
-        final DoubleUnaryOperator f8 = new CompressedExponentialFunction(1, 1);
+        final DoubleUnaryOperator f8 = new CompressedExponentialFunction(1, 3);
         assertEquals(0, f8.applyAsDouble(0), EPSILON);
-        assertEquals(0.632120558828558, f8.applyAsDouble(1), EPSILON);
-        assertEquals(0.864664716763387, f8.applyAsDouble(2), EPSILON);
+        assertEquals(0.6321, f8.applyAsDouble(1), EPSILON);
+        assertEquals(1, f8.applyAsDouble(2), EPSILON);
 
-        final DoubleUnaryOperator f9 = new CompressedExponentialFunction(1, 2);
+        final DoubleUnaryOperator f9 = new CompressedExponentialFunction(1, 4);
         assertEquals(0, f9.applyAsDouble(0), EPSILON);
-        assertEquals(0.632120558828558, f9.applyAsDouble(1), EPSILON);
-        assertEquals(0.981684361111266, f9.applyAsDouble(2), EPSILON);
+        assertEquals(0.6321, f9.applyAsDouble(1), EPSILON);
+        assertEquals(1, f9.applyAsDouble(2), EPSILON);
     }
 }
