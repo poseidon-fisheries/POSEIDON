67f4df719d971c0fad398ba0ce50007f6975363c
Nicolas Payette <nicolaspayette@gmail.com>
2022-10-04 17:28:48 +0100

    use Guava cache instead of Locker for SpecificProtectedAreaFactory

--- src/main/java/uk/ac/ox/oxfish/model/regs/factory/SpecificProtectedAreaFactory.java
+++ src/main/java/uk/ac/ox/oxfish/model/regs/factory/SpecificProtectedAreaFactory.java
@@ -2,30 +2,29 @@ package uk.ac.ox.oxfish.model.regs.factory;
 
 import com.google.common.collect.ImmutableSet;
 import sim.util.geo.MasonGeometry;
+import uk.ac.ox.oxfish.fisher.purseseiner.caches.CacheByFishState;
 import uk.ac.ox.oxfish.geography.NauticalMap;
 import uk.ac.ox.oxfish.model.FishState;
 import uk.ac.ox.oxfish.model.regs.SpecificProtectedArea;
 import uk.ac.ox.oxfish.utility.AlgorithmFactory;
-import uk.ac.ox.oxfish.utility.Locker;
 
 abstract public class SpecificProtectedAreaFactory implements AlgorithmFactory<SpecificProtectedArea> {
 
-    private final Locker<String, SpecificProtectedArea> specificProtectedAreaLocker = new Locker<>();
-
-    abstract ImmutableSet<MasonGeometry> buildGeometries(FishState fishState);
-
-    private SpecificProtectedArea buildSpecificProtectedArea(FishState fishState) {
-        ImmutableSet<MasonGeometry> geometries = buildGeometries(fishState);
-        fishState.registerStartable(model -> {
-            final NauticalMap map = model.getMap();
-            geometries.forEach(map.getMpaVectorField()::addGeometry);
-            map.recomputeTilesMPA(); // this destroys MPAs created through StartingMPA::buildMPA
+    private final CacheByFishState<SpecificProtectedArea> cache =
+        new CacheByFishState<>(fishState -> {
+            ImmutableSet<MasonGeometry> geometries = buildGeometries(fishState);
+            fishState.registerStartable(model -> {
+                final NauticalMap map = model.getMap();
+                geometries.forEach(map.getMpaVectorField()::addGeometry);
+                map.recomputeTilesMPA(); // this destroys MPAs created through StartingMPA::buildMPA
+            });
+            return new SpecificProtectedArea(geometries);
         });
-        return new SpecificProtectedArea(geometries);
-    }
 
     @Override
     public SpecificProtectedArea apply(FishState fishState) {
-        return specificProtectedAreaLocker.presentKey(fishState.getHopefullyUniqueID(), () -> buildSpecificProtectedArea(fishState));
+        return cache.get(fishState);
     }
+
+    abstract ImmutableSet<MasonGeometry> buildGeometries(FishState fishState);
 }
