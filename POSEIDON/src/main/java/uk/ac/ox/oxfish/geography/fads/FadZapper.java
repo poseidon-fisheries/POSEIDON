057259a7f4eeaa4eee2719a310f6be9cf4e8a440
Nicolas Payette <nicolaspayette@gmail.com>
2023-04-05 17:07:05 +0100

    clean FadZapper class

--- POSEIDON/src/main/java/uk/ac/ox/oxfish/geography/fads/FadZapper.java
+++ POSEIDON/src/main/java/uk/ac/ox/oxfish/geography/fads/FadZapper.java
@@ -3,48 +3,40 @@ package uk.ac.ox.oxfish.geography.fads;
 import sim.engine.SimState;
 import sim.engine.Steppable;
 import uk.ac.ox.oxfish.fisher.purseseiner.fads.AbstractFad;
-import uk.ac.ox.oxfish.fisher.purseseiner.fads.Fad;
 import uk.ac.ox.oxfish.model.AdditionalStartable;
 import uk.ac.ox.oxfish.model.FishState;
 import uk.ac.ox.oxfish.model.StepOrder;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * checks each step all fads that pass a predicate; if they do, then lose them
  */
 public class FadZapper implements Steppable, AdditionalStartable {
 
+    private final Predicate<? super AbstractFad<?, ?>> validator;
 
-    private final Predicate<AbstractFad> validator;
-
-    public FadZapper(Predicate<AbstractFad> validator) {
+    public FadZapper(final Predicate<? super AbstractFad<?, ?>> validator) {
         this.validator = validator;
     }
 
     @Override
-    public void step(SimState simState) {
-        FadMap fadMap = ((FishState) simState).getFadMap();
-        List<AbstractFad> listToRemove = new LinkedList<>();
-        for (Object o : fadMap.allFadsAsList()) {
-            if(o instanceof Fad && validator.test(((AbstractFad) o)))
-            {
-                listToRemove.add((AbstractFad) o);
-            }
-        }
-        for (AbstractFad fad : listToRemove) {
-            fadMap.destroyFad(fad);
-            fad.releaseFish(((FishState) simState).getSpecies());
-
-        }
-
+    public void step(final SimState simState) {
+        Optional.ofNullable(((FishState) simState).getFadMap()).ifPresent(fadMap ->
+            fadMap.allFads().collect(toList()).stream()
+                .filter(validator)
+                .forEach(fad -> {
+                    fadMap.destroyFad(fad);
+                    fad.releaseFish(((FishState) simState).getSpecies());
+                })
+        );
     }
 
-
     @Override
-    public void start(FishState model) {
+    public void start(final FishState model) {
         model.scheduleEveryDay(this, StepOrder.DAWN);
     }
 }
