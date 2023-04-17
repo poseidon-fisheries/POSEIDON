fd08537083307909f3dc9f99556e64dca2d50614
Nicolas Payette <nicolaspayette@gmail.com>
2023-04-17 13:55:09 +0100

    use JCommander for TwoPunchCalibration CLI

--- POSEIDON/src/main/java/uk/ac/ox/oxfish/maximization/TwoPunchCalibration.java
+++ POSEIDON/src/main/java/uk/ac/ox/oxfish/maximization/TwoPunchCalibration.java
@@ -1,30 +1,42 @@
 package uk.ac.ox.oxfish.maximization;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.converters.PathConverter;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
+
+import static java.lang.Runtime.getRuntime;
 
 public class TwoPunchCalibration {
 
+    @Parameter(names = {"-p", "--parallel_threads"})
+    private int parallelThreads = getRuntime().availableProcessors();
+
+    @Parameter(names = {"-g", "--max_global_calls"})
+    private int maxGlobalCalls = 2000;
+
+    @Parameter(names = {"-l", "--max_local_calls"})
+    private int maxLocalCalls = 5000;
+
+    @Parameter(converter = PathConverter.class)
+    private Path calibrationFile;
+
     public static void main(final String[] args) throws IOException {
-        runAll(
-            Paths.get(args[0]),
-            Integer.parseInt(args[1]),
-            args.length > 2 ? Integer.parseInt(args[2]) : 2000,
-            args.length > 3 ? Integer.parseInt(args[3]) : 5000
-        );
+        final TwoPunchCalibration twoPunchCalibration = new TwoPunchCalibration();
+        JCommander.newBuilder()
+            .addObject(twoPunchCalibration)
+            .build()
+            .parse(args);
+        twoPunchCalibration.runAll();
     }
 
-    private static void runAll(
-        final Path calibrationFile,
-        final int nProcs,
-        final int stepOneFitnessCalls,
-        final int stepTwoFitnessCalls
-    ) throws IOException {
+    private void runAll() throws IOException {
         //run GA
-        final double[] gaSolution = stepOne(calibrationFile, nProcs, stepOneFitnessCalls);
+        final double[] gaSolution = stepOne();
         writeSolutionOut(calibrationFile, gaSolution, "ga_solution.txt");
         final double[] zeros = new double[gaSolution.length];
         Arrays.fill(zeros, 0d);
@@ -37,11 +49,7 @@ public class TwoPunchCalibration {
             .2
         );
         final Path localCalibrationFile = calibrationFile.getParent().resolve("local_calibration.yaml");
-        final double[] localSolution = stepTwo(
-            localCalibrationFile,
-            nProcs,
-            stepTwoFitnessCalls
-        );
+        final double[] localSolution = stepTwo();
         writeSolutionOut(localCalibrationFile, localSolution, "local_solution.txt");
 
         //run once again locally
@@ -49,27 +57,20 @@ public class TwoPunchCalibration {
         evaluator.setNumRuns(10);
         evaluator.setParallel(false);
         evaluator.run();
-
-
     }
 
-    private static double[] stepOne(
-        final Path calibrationFile,
-        final int nProcs,
-        final int maxFitnessCalls
-    ) throws IOException {
+    private double[] stepOne() throws IOException {
 
         final TunaCalibrationConsole firstStep = new TunaCalibrationConsole();
         firstStep.setLocalSearch(false);
         firstStep.setPSO(false);
         firstStep.setPopulationSize(200);
-        firstStep.setMaxProcessorsToUse(nProcs);
+        firstStep.setMaxProcessorsToUse(parallelThreads);
         firstStep.setNumberOfRunsPerSettingOverride(1);
-        firstStep.setMaxFitnessCalls(maxFitnessCalls);
+        firstStep.setMaxFitnessCalls(maxGlobalCalls);
         firstStep.setParameterRange(15);
         firstStep.setRunNickName("global");
         firstStep.setPathToCalibrationYaml(calibrationFile.toAbsolutePath().toString());
-//        --pop 200 --maxProcs 4 --runs 3000 --range 10 --runsPerSetting 1
         return firstStep.generateCalibratorProblem().run();
     }
 
@@ -87,12 +88,7 @@ public class TwoPunchCalibration {
         writer.close();
     }
 
-    public static double[] stepTwo(
-        final Path calibrationFile,
-        final int nProcs,
-        final int maxFitnessCalls
-    ) throws IOException {
-
+    private double[] stepTwo() throws IOException {
         final TunaCalibrationConsole secondStep = new TunaCalibrationConsole();
         secondStep.setLocalSearch(false);
         secondStep.setPSO(true);
@@ -100,13 +96,50 @@ public class TwoPunchCalibration {
             calibrationFile.getParent().resolve("zeros.txt").toFile().getAbsolutePath()
         );
         secondStep.setPopulationSize(50);
-        secondStep.setMaxProcessorsToUse(nProcs);
+        secondStep.setMaxProcessorsToUse(parallelThreads);
         secondStep.setNumberOfRunsPerSettingOverride(2);
-        secondStep.setMaxFitnessCalls(maxFitnessCalls);
+        secondStep.setMaxFitnessCalls(maxLocalCalls);
         secondStep.setParameterRange(17);
         secondStep.setPathToCalibrationYaml(calibrationFile.toAbsolutePath().toString());
         secondStep.setRunNickName("local");
         return secondStep.generateCalibratorProblem().run();
+    }
+
+    public int getParallelThreads() {
+        return parallelThreads;
+    }
+
+    @SuppressWarnings("unused")
+    public void setParallelThreads(final int parallelThreads) {
+        this.parallelThreads = parallelThreads;
+    }
+
+    public int getMaxLocalCalls() {
+        return maxLocalCalls;
+    }
+
+    @SuppressWarnings("unused")
+    public void setMaxLocalCalls(final int maxLocalCalls) {
+        this.maxLocalCalls = maxLocalCalls;
+    }
+
+    public int getMaxGlobalCalls() {
+        return maxGlobalCalls;
+    }
+
+    @SuppressWarnings("unused")
+    public void setMaxGlobalCalls(final int maxGlobalCalls) {
+        this.maxGlobalCalls = maxGlobalCalls;
+    }
+
+    @SuppressWarnings("unused")
+    public Path getCalibrationFile() {
+        return calibrationFile;
+    }
+
+    @SuppressWarnings("unused")
+    public void setCalibrationFile(final Path calibrationFile) {
+        this.calibrationFile = calibrationFile;
     }
 
 }
