f4a18f7aa4168d3527c0b5a55fa6e483d3ee6a73
Nicolas Payette <nicolaspayette@gmail.com>
2023-11-27 17:45:49 +0000

    add equals and hashchode to FixedParameter

--- POSEIDON/src/main/java/uk/ac/ox/oxfish/utility/parameters/FixedParameter.java
+++ POSEIDON/src/main/java/uk/ac/ox/oxfish/utility/parameters/FixedParameter.java
@@ -1,10 +1,20 @@
 package uk.ac.ox.oxfish.utility.parameters;
 
+import java.util.Objects;
+
 public class FixedParameter<T> implements Parameter {
     private T value;
 
     FixedParameter(final T value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FixedParameter)) return false;
+        final FixedParameter<?> that = (FixedParameter<?>) o;
+        return Objects.equals(getValue(), that.getValue());
     }
 
     public T getValue() {
@@ -13,5 +23,10 @@ public class FixedParameter<T> implements Parameter {
 
     public void setValue(final T value) {
         this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
     }
 }
