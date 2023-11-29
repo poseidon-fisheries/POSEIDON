poseidon-fisheries/POSEIDON

commit 5e2035e57402340ad3847dd2ce9c7d4d2cbae7ab

filter out illegal FAD sets before trying to add them to a plan
See comments in the code, but we believe this is the cause of our recent
calibrations overestimating SKJ catch on OFS, because FADs in the west
were never being set on by their owners, since trying to plan for sets
on own FADs in that region triggered the "don't add any more of those"
mechanism.

POSEIDON/src/main/java/uk/ac/ox/oxfish/fisher/purseseiner/planner/DrawThenCheapestInsertionPlanner.java
@@ -317,6 +317,11 @@ public class DrawThenCheapestInsertionPlanner implements FisherStartable {
 
         //if the planning module cannot propose more actions, ignore them for this plan
         if (plannedAction == null || (doNotWaitToPurgeIllegalActions && !plannedAction.isAllowedNow(fisher))) {
+            // If there is an attempt to add an illegal action to the plan, we don't allow any more actions of
+            // that type to be added. We're leaving that bit of code in for now, but we're questioning whether
+            // it's necessary. It caused problem when attempts to add FAD sets in, e.g., EEZs; preventing further
+            // FAD sets to be added. We fixed that by filtering out illegal FAD sets "upstream" (in the
+            // OwnFadSetDiscretizedActionGenerator) but we should be careful with this -- NP 2023-11-14.
             stillAllowedActionsInPlan.get(nextActionType).setValue(0);
             //try planning more
             return planRecursively(currentPlan, hoursLeftInBudget, model, fisher);

POSEIDON/src/main/java/uk/ac/ox/oxfish/fisher/purseseiner/planner/OwnFadSetDiscretizedActionGenerator.java
@@ -43,10 +43,10 @@ public class OwnFadSetDiscretizedActionGenerator {
     private PriorityQueue<ValuedFad>[] rankedFads;
     /**
      * when this is set to true, the generator will immediately remove all fads that are currently not
-     * allowed to be caught. This is set to false because the standard procedure is to wait till the action
-     * comes up in the plan before checking its validity.
+     * allowed to be caught. This is needs to be true, otherwise DrawThenCheapestInsertionPlanner will
+     * prevent further FAD sets to be added to the plan if we try to add a set on an illegal FAD.
      */
-    private boolean filterOutCurrentlyInvalidFads = false;
+    private boolean filterOutCurrentlyInvalidFads = true;
 
     //todo add minimum soaktime
 

commit 9d83c1a8bf32f8ea8c0fc734ff80f587a9d4a953

fix OwnFadSetDiscretizedActionGeneratorTest
was broken by 5e2035e57402340ad3847dd2ce9c7d4d2cbae7ab

POSEIDON/src/test/java/uk/ac/ox/oxfish/fisher/purseseiner/planner/OwnFadSetDiscretizedActionGeneratorTest.java
@@ -76,6 +76,8 @@ class OwnFadSetDiscretizedActionGeneratorTest {
         when(fadManager.getFadMap()).thenReturn(fadMap);
         when(fadManager.getFisher()).thenReturn(fisher);
 
+        when(fisher.isAllowedAtSea()).thenReturn(true);
+        when(fisher.isAllowedToFishHere(any(), any())).thenReturn(true);
         when(fisher.getGear()).thenReturn(gear);
         when(fisher.grabState()).thenReturn(fishState);
         when(fishState.getMap()).thenReturn(map);
@@ -162,6 +164,8 @@ class OwnFadSetDiscretizedActionGeneratorTest {
         when(gear.isSafe(any())).thenReturn(true);
         final FadManager fadManager = gear.getFadManager();
         when(fadManager.getFisher()).thenReturn(fisher);
+        when(fisher.isAllowedAtSea()).thenReturn(true);
+        when(fisher.isAllowedToFishHere(any(), any())).thenReturn(true);
         when(fisher.getGear()).thenReturn(gear);
         when(fisher.grabState()).thenReturn(fishState);
         when(fishState.getMap()).thenReturn(map);
@@ -223,6 +227,8 @@ class OwnFadSetDiscretizedActionGeneratorTest {
             );
         final FadManager fadManager = gear.getFadManager();
         when(fadManager.getFisher()).thenReturn(fisher);
+        when(fisher.isAllowedAtSea()).thenReturn(true);
+        when(fisher.isAllowedToFishHere(any(), any())).thenReturn(true);
         when(fisher.getGear()).thenReturn(gear);
         when(fisher.grabState()).thenReturn(fishState);
         when(fishState.getMap()).thenReturn(map);