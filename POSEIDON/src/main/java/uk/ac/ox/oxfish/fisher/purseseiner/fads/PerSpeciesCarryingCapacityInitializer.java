e5c2622c3ba3f94c54521f655493837f3a6831ab
Nicolas Payette <nicolaspayette@gmail.com>
2023-12-14 15:56:40 +0000

    make sure FAD carrying capacities are never all zeros

--- POSEIDON/src/main/java/uk/ac/ox/oxfish/fisher/purseseiner/fads/PerSpeciesCarryingCapacityInitializer.java
+++ POSEIDON/src/main/java/uk/ac/ox/oxfish/fisher/purseseiner/fads/PerSpeciesCarryingCapacityInitializer.java
@@ -5,6 +5,7 @@ import uk.ac.ox.oxfish.biology.Species;
 import uk.ac.ox.oxfish.utility.parameters.DoubleParameter;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 
@@ -19,16 +20,24 @@ public class PerSpeciesCarryingCapacityInitializer
         this.carryingCapacities = carryingCapacities;
     }
 
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
     @Override
     public PerSpeciesCarryingCapacity apply(final MersenneTwisterFast rng) {
-        return new PerSpeciesCarryingCapacity(
-            carryingCapacities
-                .entrySet()
-                .stream()
-                .collect(toImmutableMap(
-                    Map.Entry::getKey,
-                    entry -> entry.getValue().applyAsDouble(rng)
-                ))
-        );
+        // generate maps of carrying capacities per species until we find one where there
+        // is at least one species for which the carrying capacity is greater than zero
+        // and use that map to construct the `PerSpeciesCarryingCapacity` object
+        return Stream.<Map<Species, Double>>generate(() ->
+                carryingCapacities
+                    .entrySet()
+                    .stream()
+                    .collect(toImmutableMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue().applyAsDouble(rng)
+                    ))
+            )
+            .filter(carryingCapacityMap -> carryingCapacityMap.values().stream().anyMatch(cc -> cc > 0))
+            .map(PerSpeciesCarryingCapacity::new)
+            .findFirst()
+            .get();
     }
 }
