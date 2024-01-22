48aa3017f87555d66854a91e343d8b78ac4891d0
Nicolas Payette <nicolaspayette@gmail.com>
2024-01-22 13:52:13 +0000

    fix seed in TwoSpeciesITQSplit test

--- POSEIDON/src/test/java/uk/ac/ox/oxfish/demoes/TwoSpeciesITQSplit.java
+++ POSEIDON/src/test/java/uk/ac/ox/oxfish/demoes/TwoSpeciesITQSplit.java
@@ -20,7 +20,6 @@
 
 package uk.ac.ox.oxfish.demoes;
 
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import uk.ac.ox.oxfish.biology.growers.SimpleLogisticGrowerFactory;
@@ -38,34 +37,30 @@ import java.nio.file.Paths;
 
 public class TwoSpeciesITQSplit {
 
-
     /**
-     * 2 species ITQ, both are valuable but the quotas of the ones only available south are very few so that
-     * it's better to fish north. The results are muffled by the fact that over time north gets consumed and it becomes better
-     * to fish south instead anyway.
+     * 2 species ITQ, both are valuable but the quotas of the ones only available south are very few so that it's better
+     * to fish north. The results are muffled by the fact that over time north gets consumed and it becomes better to
+     * fish south instead anyway.
      */
     @Test
     public void itqAffectsGeography() throws Exception {
 
-
-        FishYAML yaml = new FishYAML();
-        String scenarioYaml = String.join("\n", Files.readAllLines(
+        final FishYAML yaml = new FishYAML();
+        final String scenarioYaml = String.join("\n", Files.readAllLines(
             Paths.get("inputs", "first_paper", "location_itq.yaml")));
-        PrototypeScenario scenario = yaml.loadAs(scenarioYaml, PrototypeScenario.class);
-        FishState state = new FishState();
+        final PrototypeScenario scenario = yaml.loadAs(scenarioYaml, PrototypeScenario.class);
+        final FishState state = new FishState();
         state.setScenario(scenario);
-
 
         long towsNorth = 0;
         long towsSouth = 0;
 
         state.start();
-        //first year, just lspiRun: there is no ITQ running anyway
+        // first year, just lspiRun: there is no ITQ running anyway
         while (state.getYear() < 1) {
             state.schedule.step(state);
         }
         state.schedule.step(state);
-
 
         while (state.getYear() < 5) {
             state.schedule.step(state);
@@ -82,20 +77,19 @@ public class TwoSpeciesITQSplit {
         System.out.println("North vs South : " + towsNorth / ((double) towsNorth + towsSouth));
         Assertions.assertTrue(towsNorth / ((double) towsNorth + towsSouth) > .6);
 
-
     }
 
-
     /**
-     * we make fish so mobile that the depletion of reds isn't a problem: we are going to see geography choices
-     * being as effective as switching gear
+     * we make fish so mobile that the depletion of reds isn't a problem: we are going to see geography choices being as
+     * effective as switching gear
      */
     @Test
     public void TwoSpeciesITQSplitUnmuffled() {
-        //this I think has about a 2% failure rate; I am going to make it run twice. If it fails twice, we have a problem
+        // this I think has about a 2% failure rate; I am going to make it run twice. If it fails twice, we have a
+        // problem
         try {
             unmuffledTestOnce();
-        } catch (AssertionError e) {
+        } catch (final AssertionError e) {
             unmuffledTestOnce();
 
         }
@@ -103,59 +97,54 @@ public class TwoSpeciesITQSplit {
 
     private void unmuffledTestOnce() {
 
+        final FishState state = new FishState(0);
+        // world split in half
 
-        final FishState state = new FishState(System.currentTimeMillis());
-        //world split in half
-
-        MultiITQFactory multiFactory = new MultiITQFactory();
-        //quota ratios: 90-10
+        final MultiITQFactory multiFactory = new MultiITQFactory();
+        // quota ratios: 90-10
         multiFactory.setQuotaFirstSpecie(new FixedDoubleParameter(4500));
         multiFactory.setQuotaOtherSpecies(new FixedDoubleParameter(500));
 
-        SplitInitializerFactory biologyFactory = new SplitInitializerFactory();
+        final SplitInitializerFactory biologyFactory = new SplitInitializerFactory();
         biologyFactory.setCarryingCapacity(new FixedDoubleParameter(5000));
         biologyFactory.setGrower(new SimpleLogisticGrowerFactory(.9));
 
-
-        PrototypeScenario scenario = new PrototypeScenario();
+        final PrototypeScenario scenario = new PrototypeScenario();
         state.setScenario(scenario);
-        //world split in half
+        // world split in half
         scenario.setBiologyInitializer(biologyFactory);
         scenario.setRegulation(multiFactory);
 
-
-        SimpleMapInitializerFactory simpleMap = new SimpleMapInitializerFactory();
+        final SimpleMapInitializerFactory simpleMap = new SimpleMapInitializerFactory();
         simpleMap.setCoastalRoughness(new FixedDoubleParameter(0d));
         scenario.setMapInitializer(simpleMap);
         scenario.forcePortPosition(new int[]{40, 25});
 
         scenario.setUsePredictors(true);
 
-
         long towsNorth = 0;
         long towsSouth = 0;
 
         state.start();
-        //first year, just lspiRun: there is no ITQ running anyway
+        // first year, just lspiRun: there is no ITQ running anyway
         while (state.getYear() < 1) {
             state.schedule.step(state);
         }
         state.schedule.step(state);
 
         state.schedule.step(state);
-        double earlyRedLandings = state.getYearlyDataSet().getLatestObservation(state.getSpecies().get(0) + " " +
+        final double earlyRedLandings = state.getYearlyDataSet().getLatestObservation(state.getSpecies().get(0) + " " +
             AbstractMarket.LANDINGS_COLUMN_NAME);
-        double earlyBlueLandings = state.getYearlyDataSet().getLatestObservation(state.getSpecies().get(1) + " " +
+        final double earlyBlueLandings = state.getYearlyDataSet().getLatestObservation(state.getSpecies().get(1) + " " +
             AbstractMarket.LANDINGS_COLUMN_NAME);
 
         System.out.println("Early Landings: " + earlyRedLandings + " --- " + earlyBlueLandings);
-        //blue start as a choke species
-        double totalBlueQuotas = 500 * 100;
+        // blue start as a choke species
+        final double totalBlueQuotas = 500 * 100;
         Assertions.assertTrue(earlyBlueLandings > .8 * totalBlueQuotas);
-        //red is underutilized
-        double totalRedQuotas = 4500 * 100;
+        // red is underutilized
+        final double totalRedQuotas = 4500 * 100;
         Assertions.assertTrue(earlyRedLandings < .5 * totalRedQuotas);
-
 
         while (state.getYear() < 5) {
             state.schedule.step(state);
@@ -172,18 +161,20 @@ public class TwoSpeciesITQSplit {
         System.out.println("North vs South : " + towsNorth / ((double) towsNorth + towsSouth));
         Assertions.assertTrue(towsNorth / ((double) towsNorth + towsSouth) > .6);
 
-        //by year 10 the quotas are very well used!
-        double lateRedLandings = state.getYearlyDataSet().getLatestObservation(state.getSpecies().get(0) + " " +
+        // by year 10 the quotas are very well used!
+        final double lateRedLandings = state.getYearlyDataSet().getLatestObservation(state.getSpecies().get(0) + " " +
             AbstractMarket.LANDINGS_COLUMN_NAME);
-        double lateBlueLandings = state.getYearlyDataSet().getLatestObservation(state.getSpecies().get(1) + " " +
+        final double lateBlueLandings = state.getYearlyDataSet().getLatestObservation(state.getSpecies().get(1) + " " +
             AbstractMarket.LANDINGS_COLUMN_NAME);
         System.out.println("Late Landings: " + lateRedLandings + " --- " + lateBlueLandings);
         System.out.println(
-            "Late Quota Efficiency: " + lateRedLandings / totalRedQuotas + " --- " + lateBlueLandings / totalBlueQuotas);
+            "Late Quota Efficiency: " +
+                lateRedLandings / totalRedQuotas +
+                " --- " +
+                lateBlueLandings / totalBlueQuotas);
 
-        //geographical choice with "fixed" biology works very strongly
+        // geographical choice with "fixed" biology works very strongly
         Assertions.assertTrue(lateRedLandings > .6 * totalRedQuotas);
     }
-
 
 }
