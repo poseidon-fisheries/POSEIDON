f5a3a6fef2416d34f25fb4dddde901b83fbcb916
Nicolas Payette <nicolaspayette@gmail.com>
2024-02-09 19:32:04 +0000

    ensure service files are merged when creating shadow JARs

--- epo/build.gradle.kts
+++ epo/build.gradle.kts
@@ -1,5 +1,14 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("poseidon.java-conventions")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        mergeServiceFiles()
+    }
 }
 
 dependencies {
--- poseidon-r/build.gradle.kts
+++ poseidon-r/build.gradle.kts
@@ -1,8 +1,15 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("poseidon.java-conventions")
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
+tasks {
+    named<ShadowJar>("shadowJar") {
+        mergeServiceFiles()
+    }
+}
 dependencies {
     implementation(project(":common"))
     implementation(project(":simulations:api"))
