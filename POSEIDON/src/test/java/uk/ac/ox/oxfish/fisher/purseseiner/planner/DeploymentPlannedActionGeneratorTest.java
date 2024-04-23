1729fe73e7f6203de094a17760e5d69473e3e652
Nicolas Payette <nicolaspayette@gmail.com>
2024-04-23 14:01:41 +0100

    fix failing test

--- POSEIDON/src/test/java/uk/ac/ox/oxfish/fisher/purseseiner/planner/DeploymentPlannedActionGeneratorTest.java
+++ POSEIDON/src/test/java/uk/ac/ox/oxfish/fisher/purseseiner/planner/DeploymentPlannedActionGeneratorTest.java
@@ -1,23 +1,25 @@
 /*
- * POSEIDON, an agent-based model of fisheries
- * Copyright (c) 2024-2024 CoHESyS Lab cohesys.lab@gmail.com
+ * POSEIDON: an agent-based model of fisheries
+ * Copyright (c) 2024 CoHESyS Lab cohesys.lab@gmail.com
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with this program.
- * If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package uk.ac.ox.oxfish.fisher.purseseiner.planner;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import ec.util.MersenneTwisterFast;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import sim.util.Int2D;
 import uk.ac.ox.oxfish.fisher.Fisher;
@@ -25,58 +27,63 @@ import uk.ac.ox.oxfish.fisher.purseseiner.equipment.PurseSeineGear;
 import uk.ac.ox.oxfish.fisher.purseseiner.strategies.fields.DeploymentLocationValues;
 import uk.ac.ox.oxfish.geography.NauticalMap;
 import uk.ac.ox.oxfish.model.FishState;
+import uk.ac.ox.poseidon.regulations.api.Regulations;
 
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 import static uk.ac.ox.oxfish.geography.TestUtilities.makeMap;
 
-public class DeploymentPlannedActionGeneratorTest {
+class DeploymentPlannedActionGeneratorTest {
 
-    @SuppressWarnings("rawtypes")
     @Test
-    public void drawsCorrectly() {
+    void drawsCorrectly() {
         final Fisher fisher = mock(Fisher.class);
+        when(fisher.isAllowedAtSea()).thenReturn(true);
         final FishState fishState = mock(FishState.class);
         final NauticalMap map = makeMap(4, 4);
         when(fisher.grabState()).thenReturn(fishState);
+        final Regulations regulations = mock(Regulations.class);
+        when(regulations.isPermitted(any())).thenReturn(true);
+        when(fishState.getRegulations()).thenReturn(regulations);
 
-        final ImmutableMap<Int2D, Double> initialValues = ImmutableMap.of(
-            new Int2D(0, 0), 0.0,
-            new Int2D(1, 1), 1.0,
-            new Int2D(2, 2), 2.0
-        );
+        final ImmutableMap<Int2D, Double> initialValues =
+            ImmutableMap.of(
+                new Int2D(0, 0), 0.0,
+                new Int2D(1, 1), 1.0,
+                new Int2D(2, 2), 2.0
+            );
 
         final DeploymentLocationValues dplValues =
             new DeploymentLocationValues(__ -> initialValues, 1.0);
 
         when(fisher.getGear()).thenReturn(mock(PurseSeineGear.class, RETURNS_DEEP_STUBS));
+
         dplValues.start(fishState, fisher);
 
-        final DeploymentPlannedActionGenerator generator = new DeploymentPlannedActionGenerator(
-            fisher, dplValues,
-            map,
-            new MersenneTwisterFast()
-        );
+        final DeploymentPlannedActionGenerator generator =
+            new DeploymentPlannedActionGenerator(
+                fisher,
+                dplValues,
+                map,
+                new MersenneTwisterFast()
+            );
         generator.init();
+
         // draw 100 new deployments
-        int timesWeDeployAt22 = 0;
-        int timesWeDeployAt11 = 0;
+        final Map<Int2D, Long> counts = Stream
+            .generate(generator::drawNewPlannedAction)
+            .map(pa -> pa.getLocation().getGridLocation())
+            .limit(100)
+            .collect(groupingBy(identity(), counting()));
 
-        for (int draws = 0; draws < 100; draws++) {
-            final PlannedAction.Deploy plannedDeploy = generator.drawNewPlannedAction();
-            if (plannedDeploy.getLocation().getGridX() == 2 && plannedDeploy.getLocation().getGridY() == 2) {
-                timesWeDeployAt22++;
-            } else {
-                if (plannedDeploy.getLocation().getGridX() == 1 && plannedDeploy.getLocation().getGridY() == 1) {
-                    timesWeDeployAt11++;
-                } else {
-                    throw new RuntimeException("Drawn the wrong area: " + plannedDeploy.getLocation());
-                }
-
-            }
-        }
-        System.out.println(timesWeDeployAt22);
-        System.out.println(timesWeDeployAt11);
-        Assertions.assertTrue(timesWeDeployAt22 > timesWeDeployAt11);
-
+        assertEquals(counts.keySet(), ImmutableSet.of(new Int2D(1, 1), new Int2D(2, 2)));
+        assertTrue(counts.get(new Int2D(1, 1)) < counts.get(new Int2D(2, 2)));
     }
 }
