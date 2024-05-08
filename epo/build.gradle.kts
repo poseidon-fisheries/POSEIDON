d3d054649ee4134725e4ad88dfaef3ceae8863fe
Nicolas Payette <nicolaspayette@gmail.com>
2024-05-08 18:11:33 +0100

    update jcommander in epo build

--- epo/build.gradle.kts
+++ epo/build.gradle.kts
@@ -13,5 +13,5 @@ tasks {
 
 dependencies {
     implementation(project(":POSEIDON"))
-    implementation("com.beust:jcommander:1.81") // to parse command line arguments
+    implementation("com.beust:jcommander:1.82") // to parse command line arguments
 }
