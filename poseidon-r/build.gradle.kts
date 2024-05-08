22c8a4c58dea5e02293447d1fde0ca01189c8e1e
Nicolas Payette <nicolaspayette@gmail.com>
2024-05-08 17:21:03 +0100

    upgrade gradle plugins

--- poseidon-r/build.gradle.kts
+++ poseidon-r/build.gradle.kts
@@ -19,7 +19,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     id("poseidon.java-conventions")
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 tasks {
