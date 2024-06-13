ed01957355e68501aafa4b7666d45fc9e8997280
Nicolas Payette <nicolaspayette@gmail.com>
2024-06-13 17:12:13 +0100

    attempt to fix intermittent failure in departsSometimes() test
    not sure, but I suspect that having `new MersenneTwisterFast()` in a
    tight loop might have something to do with it. We'll see...

--- POSEIDON/src/test/java/uk/ac/ox/oxfish/fisher/strategies/FixedProbabilityDepartingStrategyTest.java
+++ POSEIDON/src/test/java/uk/ac/ox/oxfish/fisher/strategies/FixedProbabilityDepartingStrategyTest.java
@@ -1,21 +1,19 @@
 /*
- *     POSEIDON, an agent-based model of fisheries
- *     Copyright (C) 2017  CoHESyS Lab cohesys.lab@gmail.com
+ * POSEIDON: an agent-based model of fisheries
+ * Copyright (c) 2017-2024 CoHESyS Lab cohesys.lab@gmail.com
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package uk.ac.ox.oxfish.fisher.strategies;
@@ -30,15 +28,15 @@ import uk.ac.ox.oxfish.model.FishState;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-
 public class FixedProbabilityDepartingStrategyTest {
-
 
     @Test
     public void alwaysDeparts() throws Exception {
 
-        final FixedProbabilityDepartingStrategy always = new FixedProbabilityDepartingStrategy(1.0, false);
-
+        final FixedProbabilityDepartingStrategy always = new FixedProbabilityDepartingStrategy(
+            1.0,
+            false
+        );
 
         for (int i = 0; i < 50; i++) {
             Assertions.assertTrue(always.shouldFisherLeavePort(
@@ -53,7 +51,10 @@ public class FixedProbabilityDepartingStrategyTest {
     @Test
     public void neverDeparts() throws Exception {
 
-        final FixedProbabilityDepartingStrategy never = new FixedProbabilityDepartingStrategy(0, false);
+        final FixedProbabilityDepartingStrategy never = new FixedProbabilityDepartingStrategy(
+            0,
+            false
+        );
         for (int i = 0; i < 50; i++)
             Assertions.assertFalse(never.shouldFisherLeavePort(
                 mock(Fisher.class),
@@ -63,26 +64,30 @@ public class FixedProbabilityDepartingStrategyTest {
 
     }
 
-
     @Test
     public void departsSometimes() throws Exception {
-        final FixedProbabilityDepartingStrategy sometimes = new FixedProbabilityDepartingStrategy(.5, false);
-
+        final FixedProbabilityDepartingStrategy sometimes =
+            new FixedProbabilityDepartingStrategy(.5, false);
+        final Fisher fisher = mock(Fisher.class);
+        final FishState fishState = mock(FishState.class);
+        final MersenneTwisterFast rng = new MersenneTwisterFast();
         int departures = 0;
-        for (int i = 0; i < 50; i++)
-            if (sometimes.shouldFisherLeavePort(mock(Fisher.class), mock(FishState.class), new MersenneTwisterFast()))
+        for (int i = 0; i < 50; i++) {
+            if (sometimes.shouldFisherLeavePort(fisher, fishState, rng))
                 departures++;
+        }
         Assertions.assertTrue(departures < 50);
         Assertions.assertTrue(departures > 0);
-
-
     }
 
     @Test
     public void checksOnlyOnceADay() throws Exception {
 
-        //100% probability but you keep asking the same day, you will only get one yes
-        final FixedProbabilityDepartingStrategy daily = new FixedProbabilityDepartingStrategy(1, true);
+        // 100% probability but you keep asking the same day, you will only get one yes
+        final FixedProbabilityDepartingStrategy daily = new FixedProbabilityDepartingStrategy(
+            1,
+            true
+        );
 
         final FishState model = mock(FishState.class);
         when(model.getDay()).thenReturn(1);
@@ -93,6 +98,10 @@ public class FixedProbabilityDepartingStrategyTest {
         }
         Assertions.assertEquals(1, departures);
         when(model.getDay()).thenReturn(2);
-        Assertions.assertTrue(daily.shouldFisherLeavePort(mock(Fisher.class), model, new MersenneTwisterFast()));
+        Assertions.assertTrue(daily.shouldFisherLeavePort(
+            mock(Fisher.class),
+            model,
+            new MersenneTwisterFast()
+        ));
     }
 }
