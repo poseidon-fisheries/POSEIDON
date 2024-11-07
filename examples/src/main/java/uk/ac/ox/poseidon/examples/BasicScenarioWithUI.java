802a3689379bc8c51f9fbae9811f9d08d8f0f029
Nicolas Payette <nicolaspayette@gmail.com>
2024-11-07 10:35:53 +0000

    use turku palette for biomass

--- examples/src/main/java/uk/ac/ox/poseidon/examples/BasicScenarioWithUI.java
+++ examples/src/main/java/uk/ac/ox/poseidon/examples/BasicScenarioWithUI.java
@@ -1,22 +1,3 @@
-/*
- * POSEIDON: an agent-based model of fisheries
- * Copyright (c) 2024 CoHESyS Lab cohesys.lab@gmail.com
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 package uk.ac.ox.poseidon.examples;
 
 import com.google.common.collect.ImmutableMap;
@@ -27,8 +8,7 @@ import uk.ac.ox.poseidon.gui.portrayals.*;
 import java.util.List;
 
 import static java.awt.Color.WHITE;
-import static uk.ac.ox.poseidon.gui.palettes.PaletteColorMap.IMOLA;
-import static uk.ac.ox.poseidon.gui.palettes.PaletteColorMap.OLERON;
+import static uk.ac.ox.poseidon.gui.palettes.PaletteColorMap.*;
 
 public class BasicScenarioWithUI extends ScenarioWithUI {
 
@@ -58,7 +38,7 @@ public class BasicScenarioWithUI extends ScenarioWithUI {
                         ),
                         "Species A Biomass",
                         new NumberGridWithCapacityPortrayalFactory(
-                            IMOLA,
+                            TURKU,
                             "Biomass",
                             false,
                             scenario.getBiomassGridA(),
@@ -66,7 +46,7 @@ public class BasicScenarioWithUI extends ScenarioWithUI {
                         ),
                         "Species B Biomass",
                         new NumberGridWithCapacityPortrayalFactory(
-                            IMOLA,
+                            TURKU,
                             "Biomass",
                             false,
                             scenario.getBiomassGridB(),
--- gui/src/main/java/uk/ac/ox/poseidon/gui/palettes/PaletteColorMap.java
+++ gui/src/main/java/uk/ac/ox/poseidon/gui/palettes/PaletteColorMap.java
@@ -1,22 +1,3 @@
-/*
- * POSEIDON: an agent-based model of fisheries
- * Copyright (c) 2024 CoHESyS Lab cohesys.lab@gmail.com
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 package uk.ac.ox.poseidon.gui.palettes;
 
 import com.univocity.parsers.csv.CsvParser;
@@ -37,6 +18,7 @@ public class PaletteColorMap extends AbstractColorMap {
 
     public static final String IMOLA = "imola";
     public static final String OLERON = "oleron";
+    public static final String TURKU = "turku";
     private static final Color TRANSPARENT = new Color(0, 0, 0, 0);
     private final Color[] colors;
     private final double minimum;
