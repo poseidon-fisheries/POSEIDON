3f0dd4badb51c5d030ba7b32059523c0acc3586d
Nicolas Payette <nicolaspayette@gmail.com>
2025-05-19 17:41:05 +0100

    update copyright notice

--- agents/src/main/java/uk/ac/ox/poseidon/agents/behaviours/strategy/ThereAndBackBehaviourFactory.java
+++ agents/src/main/java/uk/ac/ox/poseidon/agents/behaviours/strategy/ThereAndBackBehaviourFactory.java
@@ -1,6 +1,10 @@
 /*
  * POSEIDON: an agent-based model of fisheries
- * Copyright (c) 2025 CoHESyS Lab cohesys.lab@gmail.com
+ * Copyright (c) 2025, University of Oxford.
+ *
+ * University of Oxford means the Chancellor, Masters and Scholars of the
+ * University of Oxford, having an administrative office at Wellington
+ * Square, Oxford OX1 2JD, UK.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +18,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 package uk.ac.ox.poseidon.agents.behaviours.strategy;
