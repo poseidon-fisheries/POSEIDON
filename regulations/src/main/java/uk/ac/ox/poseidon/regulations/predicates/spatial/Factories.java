a36c0b47c5fbd82ec9674d00f96038c8bdb292c8
Nicolas Payette <nicolaspayette@gmail.com>
2026-03-03 17:17:34 +0000

    Add spatial/temporal predicate helper factories
    - add regulations.predicates.spatial.Factories with actionCellPredicate/actionCoordinatePredicate helpers
    
    - add regulations.predicates.temporal.Factories with betweenDates/betweenYearlyDates/inYear helpers
    
    - include current BetweenYearlyDatesFactory updates

--- regulations/src/main/java/uk/ac/ox/poseidon/regulations/predicates/spatial/Factories.java
+++ regulations/src/main/java/uk/ac/ox/poseidon/regulations/predicates/spatial/Factories.java
@@ -1,6 +1,6 @@
 /*
  * POSEIDON: an agent-based model of fisheries
- * Copyright (c) 2025, University of Oxford.
+ * Copyright (c) 2026, University of Oxford.
  *
  * University of Oxford means the Chancellor, Masters and Scholars of the
  * University of Oxford, having an administrative office at Wellington
@@ -20,35 +20,31 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package uk.ac.ox.poseidon.regulations.predicates.temporal;
+package uk.ac.ox.poseidon.regulations.predicates.spatial;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
+import sim.util.Int2D;
 import uk.ac.ox.poseidon.core.Factory;
-import uk.ac.ox.poseidon.core.RelativeScopeFactory;
 import uk.ac.ox.poseidon.core.scopes.Scope;
+import uk.ac.ox.poseidon.geography.Coordinate;
+import uk.ac.ox.poseidon.geography.grids.ModelGrid;
 
-import java.time.MonthDay;
+import java.util.function.Predicate;
 
-@Data
-@SuperBuilder
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-public class BetweenYearlyDatesFactory<S extends Scope>
-    extends RelativeScopeFactory<S, BetweenYearlyDates> {
+public class Factories {
 
-    private Factory<? super S, ? extends MonthDay> start;
-    private Factory<? super S, ? extends MonthDay> end;
+    private Factories() {}
 
-    @Override
-    protected BetweenYearlyDates newInstance(final S scope) {
-        return new BetweenYearlyDates(
-            start.get(scope),
-            end.get(scope)
-        );
+    public static <S extends Scope> ActionCellPredicateFactory<S> actionCellPredicate(
+        final Factory<? super S, ? extends ModelGrid> modelGrid,
+        final Factory<? super S, ? extends Predicate<Int2D>> cellPredicate
+    ) {
+        return new ActionCellPredicateFactory<>(modelGrid, cellPredicate);
     }
+
+    public static <S extends Scope> ActionCoordinatePredicateFactory<S> actionCoordinatePredicate(
+        final Factory<? super S, ? extends Predicate<Coordinate>> coordinatePredicate
+    ) {
+        return new ActionCoordinatePredicateFactory<>(coordinatePredicate);
+    }
+
 }
---
+++ regulations/src/main/java/uk/ac/ox/poseidon/regulations/predicates/temporal/Factories.java
@@ -0,0 +1,64 @@
+/*
+ * POSEIDON: an agent-based model of fisheries
+ * Copyright (c) 2026, University of Oxford.
+ *
+ * University of Oxford means the Chancellor, Masters and Scholars of the
+ * University of Oxford, having an administrative office at Wellington
+ * Square, Oxford OX1 2JD, UK.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.ox.poseidon.regulations.predicates.temporal;
+
+import uk.ac.ox.poseidon.core.Factory;
+import uk.ac.ox.poseidon.core.scopes.Scope;
+import uk.ac.ox.poseidon.core.time.MonthDayFactory;
+
+import java.time.LocalDate;
+import java.time.MonthDay;
+
+public class Factories {
+
+    private Factories() {}
+
+    public static <S extends Scope> BetweenDatesFactory<S> betweenDates(
+        final Factory<? super S, ? extends LocalDate> startDate,
+        final Factory<? super S, ? extends LocalDate> endDate
+    ) {
+        return new BetweenDatesFactory<>(startDate, endDate);
+    }
+
+    public static <S extends Scope> BetweenYearlyDatesFactory<S> betweenYearlyDates(
+        final Factory<? super S, ? extends MonthDay> start,
+        final Factory<? super S, ? extends MonthDay> end
+    ) {
+        return new BetweenYearlyDatesFactory<>(start, end);
+    }
+
+    public static BetweenYearlyDatesFactory<Scope> betweenYearlyDates(
+        final CharSequence start,
+        final CharSequence end
+    ) {
+        return new BetweenYearlyDatesFactory<>(
+            MonthDayFactory.parse(start),
+            MonthDayFactory.parse(end)
+        );
+    }
+
+    public static InYearFactory inYear(final int year) {
+        return new InYearFactory(year);
+    }
+
+}
