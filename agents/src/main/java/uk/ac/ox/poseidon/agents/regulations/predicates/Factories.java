473e24183a08f2e8f713360d172ee0549e854d3f
Nicolas Payette <nicolaspayette@gmail.com>
2026-06-08 16:51:29 +0100

    Add agents.regulations.predicates.Factories utility class

---
+++ agents/src/main/java/uk/ac/ox/poseidon/agents/regulations/predicates/Factories.java
@@ -0,0 +1,46 @@
+/*
+ * POSEIDON: an agent-based model of fisheries
+ * Copyright (c) 2026, University of Oxford.
+ *
+ * University of Oxford means the Chancellor, Masters and Scholars of the
+ * University of Oxford, having an administrative office at Wellington
+ * Square, Oxford OX1 2JD, UK.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.ox.poseidon.agents.regulations.predicates;
+
+import uk.ac.ox.poseidon.agents.regulations.actions.ExtendedFishingAction;
+import uk.ac.ox.poseidon.agents.vessels.VesselScope;
+import uk.ac.ox.poseidon.core.Factory;
+import uk.ac.ox.poseidon.geography.distance.DistanceCalculator;
+import uk.ac.ox.poseidon.geography.paths.GridPathFinder;
+import uk.ac.ox.poseidon.regulations.Regulations;
+
+public class Factories {
+
+    private Factories() {}
+
+    public static FishingLocationLegalityCheckerFactory fishingLocationLegalityChecker(
+        final Factory<
+            ? super VesselScope,
+            ? extends Regulations<? super ExtendedFishingAction>
+            > regulations,
+        final Factory<? super VesselScope, ? extends GridPathFinder> pathFinder,
+        final Factory<? super VesselScope, ? extends DistanceCalculator> distance
+    ) {
+        return new FishingLocationLegalityCheckerFactory(regulations, pathFinder, distance);
+    }
+}
