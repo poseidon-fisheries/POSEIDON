dd3c39dadd0d447641f6debd165e0668b55824d0
Nicolas Payette <nicolaspayette@gmail.com>
2026-06-19 12:57:32 +0100

    make EventAccumulator extend AbstractListener

--- core/src/main/java/uk/ac/ox/poseidon/core/events/EventAccumulator.java
+++ core/src/main/java/uk/ac/ox/poseidon/core/events/EventAccumulator.java
@@ -22,17 +22,16 @@
 
 package uk.ac.ox.poseidon.core.events;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.stream.Stream;
 
-@RequiredArgsConstructor
-public class EventAccumulator<E> implements Listener<E> {
-    @Getter
-    private final Class<E> eventClass;
+public class EventAccumulator<E> extends AbstractListener<E> {
+
     private final ArrayList<E> events = new ArrayList<>();
+
+    public EventAccumulator(final Class<E> eventClass) {
+        super(eventClass);
+    }
 
     @Override
     public void receive(final E event) {
